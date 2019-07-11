02520c330677decb01771151f55595439caf84f7
Johann Brehmer <mail@johannbrehmer.de>
2019-07-11 11:49:04 -0400

    Gaussian toy data

---
+++ generate_gaussian_data.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+import numpy as np
+import logging
+import argparse
+
+logging.basicConfig(
+    format="%(asctime)-5.5s %(name)-20.20s %(levelname)-7.7s %(message)s",
+    datefmt="%H:%M",
+    level=logging.INFO,
+)
+
+
+def generate(
+        latent_dim=2,
+        data_dim=100,
+        n_train=1000000,
+        n_test=10000,
+        base_dir=".",
+):
+    z_train = np.random.normal(0., 1., size=(latent_dim, n_train))
+    z_test = np.random.normal(0., 1., size=(latent_dim, n_test))
+    transform = np.random.normal(loc=0., scale=1., size=(data_dim, latent_dim))
+    x_train = transform.dot(z_train).T
+    x_test = transform.dot(z_test).T
+
+    np.save("{}/data/gaussian/gaussian_{}_{}_x_train.npy".format(base_dir, latent_dim, data_dim), x_train)
+    np.save("{}/data/gaussian/gaussian_{}_{}_x_test.npy".format(base_dir, latent_dim, data_dim), x_test)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", type=str, help="Model name.")
+    parser.add_argument("--latent", type=int, default=2)
+    parser.add_argument("--data", type=int, default=16)
+    parser.add_argument("--dir", type=str, default=".")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    logging.info("Hi!")
+    args = parse_args()
+    generate(args.latent, args.data, base_dir=args.dir)
+    logging.info("All done! Have a nice day!")
