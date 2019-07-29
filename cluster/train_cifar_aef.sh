3ebfc8d374ab84a7b1d54900f8c6b0b143879949
Johann <mail@johannbrehmer.de>
2019-07-29 16:40:31 -0400

    Fixed scripts

--- cluster/train_cifar_aef.sh
+++ cluster/train_cifar_aef.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=aef-t-cifar
-#SBATCH --output=log_train_cifar_flow.log
+#SBATCH --output=log_train_cifar_aef_%a.log
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32GB
@@ -9,6 +9,6 @@
 #SBATCH --gres=gpu:1
 
 source activate madminer
-cd /scratch/jb6504/autoencoded-flow/
+cd /scratch/jb6504/autoencoded-flow2/
 
-python -u train.py cifar_${SLURM_ARRAY_TASK_ID} --latent ${SLURM_ARRAY_TASK_ID} --dataset cifar --dir /scratch/jb6504/autoencoded-flow --outer 3 --inner 10 --debug
+python -u train.py cifar_${SLURM_ARRAY_TASK_ID} --latent ${SLURM_ARRAY_TASK_ID} --dataset cifar --dir /scratch/jb6504/autoencoded-flow2 --outer 3 --inner 10 --lr 1.e-4 --debug
