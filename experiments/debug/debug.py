Fixed bugs in JTJ calculation

experiments/debug/debug.py
@@ -15,7 +15,7 @@ for key in logging.Logger.manager.loggerDict:
     if "manifold_flow" not in key:
         logging.getLogger(key).setLevel(logging.WARNING)
 
-n = 1000
+n = 1
 x0 = torch.randn(n).view(-1,1)
 x1 = 0.5*(1.5 + x0)*(1.5-x0)
 x = torch.cat([x0,x1],1)

manifold_flow/transforms/permutations.py
@@ -1,6 +1,7 @@
 """Implementations of permutation-like transforms."""
 
 import torch
+import numpy as np
 
 from manifold_flow import utils, transforms
 
@@ -56,7 +57,7 @@ class Permutation(transforms.Transform):
             jacobian = torch.ones(outputs.size() + inputs.size()[1:]) * jacobian
 
             # Finally, view it as a (batch, n, n) Jacobian
-            jacobian = jacobian.view((inputs.size()[0], torch.prod(inputs.size()[1:]), torch.prod(inputs.size()[1:])))
+            jacobian = jacobian.view((inputs.size()[0], np.prod(inputs.size()[1:]), np.prod(inputs.size()[1:])))
 
             return outputs, jacobian
         else: