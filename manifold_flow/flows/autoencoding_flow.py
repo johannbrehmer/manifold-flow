Fixed Jacobian calculation, first checks now succesful

manifold_flow/flows/autoencoding_flow.py
@@ -147,7 +147,7 @@ class TwoStepAutoencodingFlow(nn.Module):
     def _log_prob(self, u, log_det_inner, jacobian_outer):
         jacobian_outer = jacobian_outer[:, :, :self.latent_dim]
         jtj = torch.bmm(torch.transpose(jacobian_outer,-2,-1), jacobian_outer)
-        log_det_outer = -0.5 * torch.logdet(jtj)
+        log_det_outer = -0.5 * torch.slogdet(jtj)[1]
 
         log_prob = self.latent_distribution._log_prob(u, context=None)
         log_prob = log_prob + log_det_outer + log_det_inner

manifold_flow/transforms/base.py
@@ -3,8 +3,13 @@
 import numpy as np
 import torch
 from torch import nn
+import logging
+from torch.utils.tensorboard import SummaryWriter
+from torchviz import make_dot
 
 from manifold_flow import utils
+
+logger = logging.getLogger(__name__)
 
 
 class InverseNotAvailable(Exception):
@@ -47,7 +52,29 @@ class CompositeTransform(Transform):
         if full_jacobian:
             total_jacobian = None
             for func in funcs:
-                outputs, jacobian = func(outputs, context, full_jacobian=True)
+                inputs = outputs
+                outputs, jacobian = func(inputs, context, full_jacobian=True)
+
+                # Debug computational graph
+                class Wrapper(nn.Module):
+                    def __init__(self, model):
+                        super().__init__()
+                        self.model = model
+
+                    def forward(self, inputs):
+                        return self.model(inputs, full_jacobian=False)[0]
+
+                wrapped_module = Wrapper(func)
+                log_filename = "/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow/experiments/debug/log/{}".format(type(func).__name__)
+                with SummaryWriter(log_filename) as writer:
+                    writer.add_graph(wrapped_module, inputs)
+                logger.debug("Wrote tensorboard to %s", log_filename)
+
+                # Cross-check for debugging
+                _, logabsdet = func(inputs, context, full_jacobian=False)
+                _, logabsdet_from_jacobian = torch.slogdet(jacobian)
+                logger.debug("Transformation %s has Jacobian\n%s\nwith log abs det %s (ground truth %s)", type(func).__name__, jacobian.detach().numpy()[0], logabsdet_from_jacobian[0].item(), logabsdet[0].item())
+
                 total_jacobian = jacobian if total_jacobian is None else torch.bmm(jacobian, total_jacobian)
             return outputs, total_jacobian
 

manifold_flow/transforms/coupling.py
@@ -73,13 +73,23 @@ class CouplingTransform(transforms.Transform):
         identity_split = inputs[:, self.identity_features, ...]
         transform_split = inputs[:, self.transform_features, ...]
 
+        # logger.debug("identity_split depends on inputs: %s", utils.check_dependence(identity_split, inputs))
+        # logger.debug("transform_split depends on inputs: %s", utils.check_dependence(transform_split, inputs))
+
         if full_jacobian:
             transform_params = self.transform_net(identity_split, context)
+
+            logger.debug("transform_params depends on inputs: %s", utils.check_dependence(transform_params, inputs))
+
             transform_split, _ = self._coupling_transform_forward(
                 inputs=transform_split,
                 transform_params=transform_params
             )
-            jacobian_transform = utils.batch_jacobian(transform_split, identity_split)
+            # logger.debug("transform_split depends on inputs: %s", utils.check_dependence(transform_split, inputs))
+            # logger.debug("Jacobian: %s", utils.calculate_jacobian(transform_split, inputs))
+            # logger.debug("Batch Jacobian: %s", utils.batch_jacobian(transform_split, inputs))
+
+            jacobian_transform = utils.batch_jacobian(transform_split, inputs)
 
             if self.unconditional_transform is not None:
                 identity_split, jacobian_identity = self.unconditional_transform(identity_split, context)
@@ -139,13 +149,12 @@ class CouplingTransform(transforms.Transform):
                 jacobian_identity = torch.eye(self.num_identity_features).unsqueeze(0)  # (1, n, n)
 
             transform_params = self.transform_net(identity_split, context)
-            jacobian_transform = utils.batch_jacobian(transform_params, identity_split)
 
             transform_split, _ = self._coupling_transform_inverse(
                 inputs=transform_split,
                 transform_params=transform_params,
             )
-            jacobian_transform = utils.batch_jacobian(transform_split, identity_split)
+            jacobian_transform = utils.batch_jacobian(transform_split, inputs)
 
             # Put together full Jacobian\
             batchsize = inputs.size

manifold_flow/utils.py
@@ -7,7 +7,19 @@ import torch
 logger = logging.getLogger(__name__)
 
 
-def calculate_jacobian(outputs, inputs, create_graph=False):
+def check_dependence(outputs, inputs):
+    try:
+        for i, out in enumerate(outputs.view(-1)):
+            col_i = torch.autograd.grad(out, inputs, retain_graph=True, create_graph=False, allow_unused=True)[0]
+            if col_i is not None and np.max(np.abs(col_i.detach().numpy())) > 1.e-9:
+                return True
+        return False
+    except RuntimeError as e:
+        logger.debug("%s", e)
+        return False
+
+
+def calculate_jacobian(outputs, inputs, create_graph=True):
     """Computes the jacobian of outputs with respect to inputs.
 
     Based on gelijergensen's code at https://gist.github.com/sbarratt/37356c46ad1350d4c30aefbd488a4faa.
@@ -33,7 +45,7 @@ def calculate_jacobian(outputs, inputs, create_graph=False):
     return jac.view(outputs.size() + inputs.size())
 
 
-def batch_jacobian(outputs, inputs, create_graph=False):
+def batch_jacobian(outputs, inputs, create_graph=True):
     """Computes the jacobian of outputs with respect to inputs, assuming the first dimension of both are the minibatch.
 
     Based on gelijergensen's code at https://gist.github.com/sbarratt/37356c46ad1350d4c30aefbd488a4faa.
@@ -45,12 +57,8 @@ def batch_jacobian(outputs, inputs, create_graph=False):
         jacobian of outputs with respect to inputs
     """
 
-    assert outputs.size(0) == inputs.size(0)
-
-    outputs = outputs.view((outputs.size(0), -1))
-    inputs = inputs.view((inputs.size(0), -1))
-
     jac = calculate_jacobian(outputs, inputs)
+    jac = jac.view((outputs.size(0), np.prod(outputs.size()[1:]), inputs.size(0), np.prod(inputs.size()[1:]), ))
     jac = torch.einsum("bibj->bij", jac)
 
     if create_graph: