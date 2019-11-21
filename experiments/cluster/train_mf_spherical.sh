85fafe514e26d91262d988564cb9da20f26024f7
Johann Brehmer <mail@johannbrehmer.de>
2019-11-21 15:50:34 -0500

    More fixes in SLURM scripts

--- experiments/cluster/train_mf_spherical.sh
+++ experiments/cluster/train_mf_spherical.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=mf-t-pie-sg
-#SBATCH --output=log_train_pie_spherical_%a.log
+#SBATCH --job-name=mf-t-mf-sg
+#SBATCH --output=log_train_mf_spherical_%a.log
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32GB
@@ -9,19 +9,19 @@
 #SBATCH --gres=gpu:1
 
 source activate ml
-cd /scratch/jb6504/manifold-flow
+cd /scratch/jb6504/manifold-flow/experiments
 
 case ${SLURM_ARRAY_TASK_ID} in
-0) python -u train.py --algorithm pie --datadim 15 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
+0) python -u train.py --algorithm mf --datadim 15 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
 
-1) python -u train.py --algorithm pie --datadim 15 --epsilon 0.001 --dir /scratch/jb6504/manifold-flow ;;
-2) python -u train.py --algorithm pie --datadim 15 --epsilon 0.003 --dir /scratch/jb6504/manifold-flow ;;
-3) python -u train.py --algorithm pie --datadim 15 --epsilon 0.03 --dir /scratch/jb6504/manifold-flow ;;
-4) python -u train.py --algorithm pie --datadim 15 --epsilon 0.1 --dir /scratch/jb6504/manifold-flow ;;
+1) python -u train.py --algorithm mf --datadim 15 --epsilon 0.001 --dir /scratch/jb6504/manifold-flow ;;
+2) python -u train.py --algorithm mf --datadim 15 --epsilon 0.003 --dir /scratch/jb6504/manifold-flow ;;
+3) python -u train.py --algorithm mf --datadim 15 --epsilon 0.03 --dir /scratch/jb6504/manifold-flow ;;
+4) python -u train.py --algorithm mf --datadim 15 --epsilon 0.1 --dir /scratch/jb6504/manifold-flow ;;
 
-5) python -u train.py --algorithm pie --datadim 11 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
-6) python -u train.py --algorithm pie --datadim 13 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
-7) python -u train.py --algorithm pie --datadim 20 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
-8) python -u train.py --algorithm pie --datadim 30 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
+5) python -u train.py --algorithm mf --datadim 11 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
+6) python -u train.py --algorithm mf --datadim 13 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
+7) python -u train.py --algorithm mf --datadim 20 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
+8) python -u train.py --algorithm mf --datadim 30 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
 *) echo "Nothing to do for job ${SLURM_ARRAY_TASK_ID}" ;;
 esac
