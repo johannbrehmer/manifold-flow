3af03495d050a3e4fcac8ea7188c1ec72a6516be
Johann Brehmer <mail@johannbrehmer.de>
2019-11-29 12:51:08 -0500

    Updated submit.sh

--- experiments/cluster/submit.sh
+++ experiments/cluster/submit.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-# sbatch simulate.sh
+sbatch simulate.sh
 
 # sbatch timing.sh
 
 # sbatch train_flow_spherical.sh
 # sbatch train_pie_spherical.sh
 # sbatch --array=0-4 train_mf_spherical.sh
-sbatch train_flow_csg.sh
-sbatch train_pie_csg.sh
-# sbatch --array=0-4 train_mf_csg.sh  # STILL MISSING
-sbatch train_opie_csg.sh
-# sbatch --array=0-4 train_omf_csg.sh  # STILL MISSING
+# sbatch train_flow_csg.sh
+# sbatch train_pie_csg.sh
+# sbatch --array=0-4 train_mf_csg.sh
+# sbatch train_opie_csg.sh
+# sbatch --array=0-4 train_omf_csg.sh  
 
 # sbatch evaluate_flow_spherical.sh
 # sbatch evaluate_pie_spherical.sh
-# sbatch evaluate_mf_spherical.sh  # STILL MISSING
-# sbatch evaluate_flow_csg.sh  # STILL MISSING
-# sbatch evaluate_pie_csg.sh  # STILL MISSING
-# sbatch evaluate_mf_csg.sh  # STILL MISSING
-# sbatch evaluate_opie_csg.sh  # STILL MISSING
-# sbatch evaluate_omf_csg.sh  # STILL MISSING
+# sbatch evaluate_mf_spherical.sh  
+# sbatch evaluate_flow_csg.sh  
+# sbatch evaluate_pie_csg.sh  
+# sbatch evaluate_mf_csg.sh  
+# sbatch evaluate_opie_csg.sh  
+# sbatch evaluate_omf_csg.sh  
