Fixing -- or braking? -- the manifold flow Jacobian

manifold_flow/transforms/coupling.py
@@ -96,19 +96,22 @@ class CouplingTransform(transforms.Transform):
 
             if self.unconditional_transform is not None:
                 identity_split, jacobian_identity = self.unconditional_transform(identity_split, context)
-            else:
-                jacobian_identity = torch.eye(self.num_identity_features).unsqueeze(0)  # (1, n, n)
+                raise NotImplementedError()
+            # else:
+            #     jacobian_identity = torch.eye(self.num_identity_features).unsqueeze(0)  # (1, n, n)
+            #     logger.debug("Identity Jacobian: %s", jacobian_identity[0])
 
             # Put together full Jacobian
             batchsize = inputs.size(0)
             jacobian = torch.zeros((batchsize,) + inputs.size()[1:] + inputs.size()[1:])
-            (jacobian[:,self.identity_features, :])[:,:, self.identity_features] = jacobian_identity
+            jacobian[:,self.identity_features, self.identity_features] = 1.
             jacobian[:,self.transform_features, :] = jacobian_transform
 
             outputs = torch.empty_like(inputs)
             outputs[:, self.identity_features, ...] = identity_split
             outputs[:, self.transform_features, ...] = transform_split
 
+            # logger.debug("Jacobian from coupling layer (identity features = %s): \n %s", self.identity_features, jacobian[0])
             # timer.timer(stop="Jacobian coupling transform")
 
             return outputs, jacobian

manifold_flow/transforms/permutations.py
@@ -2,10 +2,13 @@
 
 import torch
 import numpy as np
+import logging
 
 from manifold_flow import transforms
 from manifold_flow.utils import various
-from experiments.utils import timer
+
+
+logger = logging.getLogger(__name__)
 
 
 class Permutation(transforms.Transform):
@@ -40,7 +43,7 @@ class Permutation(transforms.Transform):
             # The brute force way does not seem to work, not sure why, maybe index_select breaks autodiff
             # jacobian = utils.batch_jacobian(outputs, inputs)
 
-            timer.timer(start="Jacobian permutation")
+            # timer.timer(start="Jacobian permutation")
 
             # First build the Jacobian as a 2D matrix
             jacobian = torch.zeros((outputs.size()[dim], inputs.size()[dim]))
@@ -63,7 +66,8 @@ class Permutation(transforms.Transform):
             # Finally, view it as a (batch, n, n) Jacobian
             jacobian = jacobian.view((inputs.size()[0], np.prod(inputs.size()[1:]), np.prod(inputs.size()[1:])))
 
-            timer.timer(stop="Jacobian permutation")
+            # logger.debug("Jacobian from permutation: \n %s", jacobian[0])
+            # timer.timer(stop="Jacobian permutation")
 
             return outputs, jacobian
         else: