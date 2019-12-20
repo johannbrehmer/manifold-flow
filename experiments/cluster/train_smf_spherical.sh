547ed58142faaaf26f07b863d4f8fd3e760339a1
Johann Brehmer <mail@johannbrehmer.de>
2019-12-20 12:09:23 +0100

    Specified SLURM scripts, harmonized total epochs

---
+++ experiments/cluster/train_smf_spherical.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=t-smf-sg
+#SBATCH --output=log_train_smf_sg.log
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32GB
+#SBATCH --time=7-00:00:00
+#SBATCH --gres=gpu:1
+
+module load cuda/10.1.105
+source activate ml
+cd /scratch/jb6504/manifold-flow/experiments
+
+python -u train.py --dataset spherical_gaussian --algorithm mf --specified --epsilon 0.01 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset spherical_gaussian --algorithm mf --specified --epsilon 0.001 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset spherical_gaussian --algorithm mf --specified --epsilon 0.1 --dir /scratch/jb6504/manifold-flow
---
+++ experiments/cluster/train_spie_csg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=t-spie-csg
+#SBATCH --output=log_train_spie_csg.log
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32GB
+#SBATCH --time=7-00:00:00
+#SBATCH --gres=gpu:1
+
+module load cuda/10.1.105
+source activate ml
+cd /scratch/jb6504/manifold-flow/experiments
+
+python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.01 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.001 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.1 --dir /scratch/jb6504/manifold-flow
