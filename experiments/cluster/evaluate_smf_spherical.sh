3ac18cd8422ee32704d14be9591fef7fe6ea9501
Johann Brehmer <mail@johannbrehmer.de>
2019-12-29 15:47:49 +0100

    All systems go for eval

---
+++ experiments/cluster/evaluate_smf_spherical.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=e-smf-sg
+#SBATCH --output=log_evaluate_smf_spherical.log
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32GB
+#SBATCH --time=7-00:00:00
+# #SBATCH --gres=gpu:1
+
+source activate ml
+cd /scratch/jb6504/manifold-flow/experiments
+
+python -u evaluate.py --dataset spherical_gaussian --algorithm mf --specified --outercouplingmlp --outercouplinglayers 1 --outercouplinghidden 100 --epsilon 0.01 --dir /scratch/jb6504/manifold-flow
+python -u evaluate.py --dataset spherical_gaussian --algorithm mf --specified --outercouplingmlp --outercouplinglayers 1 --outercouplinghidden 100 --epsilon 0.001 --dir /scratch/jb6504/manifold-flow
+python -u evaluate.py --dataset spherical_gaussian --algorithm mf --specified --outercouplingmlp --outercouplinglayers 1 --outercouplinghidden 100 --epsilon 0.1 --dir /scratch/jb6504/manifold-flow
