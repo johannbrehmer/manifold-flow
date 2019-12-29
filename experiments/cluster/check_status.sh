7ac5c8b0bb6a6e6bbe64146eb0b8ec6e9b193c0f
Johann Brehmer <mail@johannbrehmer.de>
2019-12-29 16:04:41 +0100

    Last update for now I swear

--- experiments/cluster/check_status.sh
+++ experiments/cluster/check_status.sh
@@ -7,7 +7,7 @@ echo ""
 for filename in log_*.log; do
     if tail -n 1 $filename | grep -q "Have a nice day"
     then
-        echo "   :)  - ${filename}"
+        echo "   :)   - ${filename}"
     else
         if grep -Fxq "Exception" $filename
         then
