0dc9b984dbc7b3ffab8e450e6afe3fff62d0a41e
Johann Brehmer <mail@johannbrehmer.de>
2020-01-06 10:02:02 +0100

    Long training runs for flow, PIE, GAMF

--- experiments/cluster/train_flow_spherical.sh
+++ experiments/cluster/train_flow_spherical.sh
@@ -12,6 +12,10 @@ module load cuda/10.1.105
 source activate ml
 cd /scratch/jb6504/manifold-flow/experiments
 
-python -u train.py --dataset spherical_gaussian --algorithm flow --epsilon 0.01 --dir /scratch/jb6504/manifold-flow
-python -u train.py --dataset spherical_gaussian --algorithm flow --epsilon 0.001 --dir /scratch/jb6504/manifold-flow
-python -u train.py --dataset spherical_gaussian --algorithm flow --epsilon 0.1 --dir /scratch/jb6504/manifold-flow
+# python -u train.py --dataset spherical_gaussian --algorithm flow --epsilon 0.01 --dir /scratch/jb6504/manifold-flow
+# python -u train.py --dataset spherical_gaussian --algorithm flow --epsilon 0.001 --dir /scratch/jb6504/manifold-flow
+# python -u train.py --dataset spherical_gaussian --algorithm flow --epsilon 0.1 --dir /scratch/jb6504/manifold-flow
+
+python -u train.py --modelname long --dataset spherical_gaussian --algorithm flow --epsilon 0.01 --epochs 200 --dir /scratch/jb6504/manifold-flow
+python -u train.py --modelname long_shallow --dataset spherical_gaussian --algorithm flow --epsilon 0.01 --epochs 200 --outerlayers 3 --innerlayers 3 --dir /scratch/jb6504/manifold-flow
+python -u train.py --modelname long_deep --dataset spherical_gaussian --algorithm flow --epsilon 0.01 --epochs 200 --outerlayers 10 --innerlayers 10 --dir /scratch/jb6504/manifold-flow
