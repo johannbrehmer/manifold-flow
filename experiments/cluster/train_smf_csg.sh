faf49a34c3ea58d2536dda1da3a84a2bbe830def
Johann <mail@johannbrehmer.de>
2020-01-06 02:52:51 -0500

    Fixed cluster scripts that confused a few things

--- experiments/cluster/train_smf_csg.sh
+++ experiments/cluster/train_smf_csg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=t-spie-csg
-#SBATCH --output=log_train_spie_csg.log
+#SBATCH --job-name=t-smf-csg
+#SBATCH --output=log_train_smf_csg.log
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32GB
@@ -12,6 +12,6 @@ module load cuda/10.1.105
 source activate ml
 cd /scratch/jb6504/manifold-flow/experiments
 
-python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.01 --dir /scratch/jb6504/manifold-flow
-python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.001 --dir /scratch/jb6504/manifold-flow
-python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.1 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset conditional_spherical_gaussian --algorithm mf --specified --epsilon 0.01 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset conditional_spherical_gaussian --algorithm mf --specified --epsilon 0.001 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset conditional_spherical_gaussian --algorithm mf --specified --epsilon 0.1 --dir /scratch/jb6504/manifold-flow
