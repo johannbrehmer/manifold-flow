64ecad323faca3913ac625d3f854066a40c50f99
Johann Brehmer <mail@johannbrehmer.de>
2020-01-14 16:45:52 -0500

    Prepared retrainineg specified PIE, added specified GAMF

--- experiments/cluster/train_spie_csg.sh
+++ experiments/cluster/train_spie_csg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=t-spie-csg
-#SBATCH --output=log_train_spie_csg_%a.log
+#SBATCH --output=log_train_spie_csg.log
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32GB
@@ -12,9 +12,7 @@ module load cuda/10.1.105
 source activate ml
 cd /scratch/jb6504/manifold-flow/experiments
 
-case ${SLURM_ARRAY_TASK_ID} in
-0) python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.01 --dir /scratch/jb6504/manifold-flow ;;
-1) python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.001 --dir /scratch/jb6504/manifold-flow ;;
-2) python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.1 --dir /scratch/jb6504/manifold-flow ;;
-*) echo "Nothing to do for job ${SLURM_ARRAY_TASK_ID}" ;;
-esac
+python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.01 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.001 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset conditional_spherical_gaussian --algorithm pie --specified --epsilon 0.1 --dir /scratch/jb6504/manifold-flow
+
