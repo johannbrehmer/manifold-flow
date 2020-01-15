f9bbf219b408ae21c59c4263f2abd6276dc69b54
Johann Brehmer <mail@johannbrehmer.de>
2020-01-15 17:17:02 -0500

    Re-prepared SMF scripts

--- experiments/cluster/submit.sh
+++ experiments/cluster/submit.sh
@@ -9,18 +9,18 @@
 # sbatch train_flow_spherical.sh  # some missing
 # sbatch train_pie_spherical.sh  # some missing
 # sbatch train_gamf_spherical.sh  # restarted with higher loss
-sbatch train_spie_spherical.sh  # not yet done
-sbatch train_sgamf_spherical.sh  # not yet done
-# sbatch --array=0-2 train_smf_spherical.sh  # not yet done (NaNs in likelihood -- fixed now?)
+# sbatch train_spie_spherical.sh  # not yet done
+# sbatch train_sgamf_spherical.sh  # not yet done
+sbatch train_smf_spherical.sh  # not yet done (NaNs in likelihood -- fixed now?)
 # sbatch --array=0-2 train_mf_spherical.sh  # not yet done
 # sbatch train_pie_epsilon_spherical.sh  # not yet done, wait for L2 results
 
 # sbatch train_flow_csg.sh  # some missing
 # sbatch train_pie_csg.sh  # some missing
 # sbatch train_gamf_csg.sh  # restarted with higher loss
-sbatch train_spie_csg.sh  # not yet done
-sbatch train_sgamf_csg.sh  # not yet done
-# sbatch --array=0-2 train_smf_csg.sh  # not yet done (NaNs in likelihood -- fixed now?)
+# sbatch train_spie_csg.sh  # not yet done
+# sbatch train_sgamf_csg.sh  # not yet done
+sbatch train_smf_csg.sh  # not yet done (NaNs in likelihood -- fixed now?)
 # sbatch --array=0-2 train_mf_csg.sh  # not yet done
 # sbatch train_pie_epsilon_csg.sh  # not yet done, wait for L2 results
 
