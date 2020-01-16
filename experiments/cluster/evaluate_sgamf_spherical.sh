4cddfc22d85d8c17cec561b0ff4572165ecabe6b
Johann Brehmer <mail@johannbrehmer.de>
2020-01-16 09:42:52 -0500

    SGAMF eval scripts

---
+++ experiments/cluster/evaluate_sgamf_spherical.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=e-sgamf-sg
+#SBATCH --output=log_evaluate_sgamf_spherical.log
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32GB
+#SBATCH --time=7-00:00:00
+# #SBATCH --gres=gpu:1
+
+source activate ml
+cd /scratch/jb6504/manifold-flow/experiments
+
+python -u evaluate.py --dataset spherical_gaussian --algorithm gamf --specified --epsilon 0.01 --dir /scratch/jb6504/manifold-flow
+python -u evaluate.py --dataset spherical_gaussian --algorithm gamf --specified --epsilon 0.001 --dir /scratch/jb6504/manifold-flow
+python -u evaluate.py --dataset spherical_gaussian --algorithm gamf --specified --epsilon 0.1 --dir /scratch/jb6504/manifold-flow
--- experiments/cluster/submit.sh
+++ experiments/cluster/submit.sh
@@ -11,7 +11,7 @@
 # sbatch train_gamf_spherical.sh  # restarted with higher loss
 # sbatch train_spie_spherical.sh  # not yet done
 # sbatch train_sgamf_spherical.sh  # not yet done
-sbatch train_smf_spherical.sh  # not yet done (NaNs in likelihood -- fixed now?)
+# sbatch train_smf_spherical.sh  # not yet done (NaNs in likelihood -- fixed now?)
 # sbatch --array=0-2 train_mf_spherical.sh  # not yet done
 # sbatch train_pie_epsilon_spherical.sh  # not yet done, wait for L2 results
 
@@ -20,7 +20,7 @@ sbatch train_smf_spherical.sh  # not yet done (NaNs in likelihood -- fixed now?)
 # sbatch train_gamf_csg.sh  # restarted with higher loss
 # sbatch train_spie_csg.sh  # not yet done
 # sbatch train_sgamf_csg.sh  # not yet done
-sbatch train_smf_csg.sh  # not yet done (NaNs in likelihood -- fixed now?)
+# sbatch train_smf_csg.sh  # not yet done (NaNs in likelihood -- fixed now?)
 # sbatch --array=0-2 train_mf_csg.sh  # not yet done
 # sbatch train_pie_epsilon_csg.sh  # not yet done, wait for L2 results
 
@@ -31,24 +31,26 @@ sbatch train_smf_csg.sh  # not yet done (NaNs in likelihood -- fixed now?)
 # sbatch train_mf_tth.sh  # CUDA memory issue, next try with bs 50
 
 
-# sbatch evaluate_flow_spherical.sh  # not yet done, some not yet trained
-# sbatch evaluate_pie_spherical.sh  # not yet done, some not yet trained
-# sbatch evaluate_gamf_spherical.sh  # not yet trained
-# sbatch evaluate_spie_spherical.sh  # not yet trained
-# sbatch evaluate_smf_spherical.sh  # not yet trained
-# sbatch evaluate_mf_spherical.sh  # not yet trained
+sbatch evaluate_flow_spherical.sh
+sbatch evaluate_pie_spherical.sh
+sbatch evaluate_gamf_spherical.sh
+sbatch evaluate_spie_spherical.sh
+sbatch evaluate_smf_spherical.sh
+sbatch evaluate_sgamf_spherical.sh
+sbatch evaluate_mf_spherical.sh
 # sbatch evaluate_pie_epsilon_spherical.sh  # not yet trained
 
-# sbatch evaluate_flow_csg.sh  # some not yet trained
-# sbatch evaluate_pie_csg.sh  # some not yet trained
-# sbatch evaluate_gamf_csg.sh  # not yet trained
-# sbatch evaluate_spie_csg.sh  # not yet trained
-# sbatch evaluate_smf_csg.sh  # not yet trained
-# sbatch evaluate_mf_csg.sh  # not yet trained
+sbatch evaluate_flow_csg.sh
+sbatch evaluate_pie_csg.sh
+sbatch evaluate_gamf_csg.sh
+sbatch evaluate_spie_csg.sh
+sbatch evaluate_smf_csg.sh
+sbatch evaluate_sgamf_csg.sh
+sbatch evaluate_mf_csg.sh
 # sbatch evaluate_pie_epsilon_csg.sh  # not yet trained
 
-# sbatch evaluate_flow_tth2d.sh  # not yet trained
-# sbatch evaluate_flow_tth.sh  # not yet done
-# sbatch evaluate_pie_tth.sh  # not yet done
-# sbatch evaluate_gamf_tth.sh  # not yet trained
+sbatch evaluate_flow_tth2d.sh
+sbatch evaluate_flow_tth.sh
+sbatch evaluate_pie_tth.sh
+sbatch evaluate_gamf_tth.sh
 # sbatch evaluate_mf_tth.sh  # not yet trained
