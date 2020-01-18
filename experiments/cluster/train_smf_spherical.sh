c4f7148f8c0ccd644c61d7f7049f1b318a58d013
Johann Brehmer <mail@johannbrehmer.de>
2020-01-18 00:24:46 -0500

    Prepared no-drouput SMF, SGAMF, SPIE training (also with new rescaling)

--- experiments/cluster/train_smf_spherical.sh
+++ experiments/cluster/train_smf_spherical.sh
@@ -12,6 +12,6 @@ module load cuda/10.1.105
 source activate ml
 cd /scratch/jb6504/manifold-flow/experiments
 
-python -u train.py --dataset spherical_gaussian --epsilon 0.01 --algorithm mf --specified --dir /scratch/jb6504/manifold-flow
-python -u train.py --dataset spherical_gaussian --epsilon 0.001 --algorithm mf --specified --dir /scratch/jb6504/manifold-flow
-python -u train.py --dataset spherical_gaussian --epsilon 0.1 --algorithm mf --specified --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset spherical_gaussian --epsilon 0.01 --algorithm mf --specified --dropout 0 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset spherical_gaussian --epsilon 0.001 --algorithm mf --specified --dropout 0 --dir /scratch/jb6504/manifold-flow
+python -u train.py --dataset spherical_gaussian --epsilon 0.1 --algorithm mf --specified --dropout 0 --dir /scratch/jb6504/manifold-flow
