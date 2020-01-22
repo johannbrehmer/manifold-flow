d7a350b76abfc55dcce3acc699ff291e6729aff8
Johann Brehmer <mail@johannbrehmer.de>
2020-01-22 12:57:12 -0500

    New ood sampling

--- experiments/cluster/submit.sh
+++ experiments/cluster/submit.sh
@@ -10,7 +10,7 @@
 # sbatch train_pie_spherical.sh
 # sbatch train_gamf_spherical.sh
 # sbatch train_spie_spherical.sh
-sbatch train_sgamf_spherical.sh
+# sbatch train_sgamf_spherical.sh
 # sbatch train_smf_spherical.sh
 # sbatch --array=0-2 train_mf_spherical.sh
 # sbatch train_pie_epsilon_spherical.sh  # not yet done, wait for L2 results
@@ -19,7 +19,7 @@ sbatch train_sgamf_spherical.sh
 # sbatch train_pie_csg.sh
 # sbatch train_gamf_csg.sh
 # sbatch train_spie_csg.sh
-sbatch train_sgamf_csg.sh
+# sbatch train_sgamf_csg.sh
 # sbatch train_smf_csg.sh
 # sbatch --array=0-2 train_mf_csg.sh
 # sbatch train_pie_epsilon_csg.sh  # not yet done, wait for L2 results
@@ -27,8 +27,8 @@ sbatch train_sgamf_csg.sh
 # sbatch train_flow_tth2d.sh
 # sbatch train_flow_tth.sh
 # sbatch train_pie_tth.sh
-sbatch train_gamf_tth.sh
-sbatch train_mf_tth.sh
+# sbatch train_gamf_tth.sh
+# sbatch train_mf_tth.sh
 
 
 sbatch evaluate_flow_spherical.sh
@@ -37,15 +37,15 @@ sbatch evaluate_gamf_spherical.sh
 sbatch evaluate_spie_spherical.sh
 sbatch evaluate_smf_spherical.sh
 sbatch evaluate_sgamf_spherical.sh
-# sbatch evaluate_mf_spherical.sh  # <---
+sbatch evaluate_mf_spherical.sh  # <---
 # sbatch evaluate_pie_epsilon_spherical.sh  # not yet trained
 
 # sbatch evaluate_flow_csg.sh
 # sbatch evaluate_pie_csg.sh
-sbatch evaluate_gamf_csg.sh
-sbatch evaluate_spie_csg.sh
+# sbatch evaluate_gamf_csg.sh
+# sbatch evaluate_spie_csg.sh
 # sbatch evaluate_smf_csg.sh
-# sbatch evaluate_sgamf_csg.sh  # <---
+sbatch evaluate_sgamf_csg.sh
 # sbatch evaluate_mf_csg.sh  # <---
 # sbatch evaluate_pie_epsilon_csg.sh  # not yet trained
 
