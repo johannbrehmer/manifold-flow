dc446b8e654630f9698e9a0eb84e386f6fbb31d8
Johann Brehmer <mail@johannbrehmer.de>
2020-01-22 13:53:44 -0500

    Let's do the time warp again

--- experiments/cluster/submit.sh
+++ experiments/cluster/submit.sh
@@ -12,7 +12,7 @@
 # sbatch train_spie_spherical.sh
 # sbatch train_sgamf_spherical.sh
 # sbatch train_smf_spherical.sh
-# sbatch --array=0-2 train_mf_spherical.sh
+sbatch --array=0-2 train_mf_spherical.sh
 # sbatch train_pie_epsilon_spherical.sh  # not yet done, wait for L2 results
 
 # sbatch train_flow_csg.sh
@@ -21,7 +21,7 @@
 # sbatch train_spie_csg.sh
 # sbatch train_sgamf_csg.sh
 # sbatch train_smf_csg.sh
-# sbatch --array=0-2 train_mf_csg.sh
+sbatch --array=0-2 train_mf_csg.sh
 # sbatch train_pie_epsilon_csg.sh  # not yet done, wait for L2 results
 
 # sbatch train_flow_tth2d.sh
@@ -37,7 +37,7 @@
 # sbatch evaluate_spie_spherical.sh
 # sbatch evaluate_smf_spherical.sh
 # sbatch evaluate_sgamf_spherical.sh
-sbatch --array 0-2 evaluate_mf_spherical.sh  # <---
+# sbatch --array 0-2 evaluate_mf_spherical.sh  # <---
 # sbatch evaluate_pie_epsilon_spherical.sh  # not yet trained
 
 # sbatch evaluate_flow_csg.sh
