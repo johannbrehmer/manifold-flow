69b2c72d875a34864ed579fde0343c6346a675b8
Johann Brehmer <mail@johannbrehmer.de>
2020-01-24 18:08:25 -0500

    Updated submit.sh

--- experiments/cluster/submit.sh
+++ experiments/cluster/submit.sh
@@ -12,7 +12,7 @@
 #sbatch train_spie_spherical.sh
 #sbatch train_sgamf_spherical.sh
 #sbatch train_smf_spherical.sh
-sbatch --array=0-2 train_mf_spherical.sh
+sbatch --array=0-5 train_mf_spherical.sh
 #sbatch train_pie_epsilon_spherical.sh
 #
 #sbatch train_flow_csg.sh
@@ -21,7 +21,7 @@ sbatch --array=0-2 train_mf_spherical.sh
 #sbatch train_spie_csg.sh
 #sbatch train_sgamf_csg.sh
 #sbatch train_smf_csg.sh
-sbatch --array=0-2 train_mf_csg.sh
+sbatch --array=0-5 train_mf_csg.sh
 #sbatch train_pie_epsilon_csg.sh
 
 #sbatch train_flow_tth2d.sh
