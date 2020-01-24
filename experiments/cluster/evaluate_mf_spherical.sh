82a9120dc9e1e0a4bbd269506d653f29102fcca9
Johann Brehmer <mail@johannbrehmer.de>
2020-01-24 10:49:49 -0500

    Prepared intermediate-state MF eval

--- experiments/cluster/evaluate_mf_spherical.sh
+++ experiments/cluster/evaluate_mf_spherical.sh
@@ -12,8 +12,24 @@ source activate ml
 cd /scratch/jb6504/manifold-flow/experiments
 
 case ${SLURM_ARRAY_TASK_ID} in
-0) python -u evaluate.py --modelname small --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.01 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
-1) python -u evaluate.py --modelname small --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.001 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
-2) python -u evaluate.py --modelname small --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.1 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
+0) python -u evaluate.py --modelname small_epoch_B9 --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.01 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
+1) python -u evaluate.py --modelname small_epoch_B9 --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.001 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
+2) python -u evaluate.py --modelname small_epoch_B9 --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.1 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
 *) echo "Nothing to do for job ${SLURM_ARRAY_TASK_ID}" ;;
 esac
+
+
+case ${SLURM_ARRAY_TASK_ID} in
+0) python -u evaluate.py --modelname small_epoch_A4 --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.01 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
+1) python -u evaluate.py --modelname small_epoch_A4 --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.001 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
+2) python -u evaluate.py --modelname small_epoch_A4 --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.1 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
+*) echo "Nothing to do for job ${SLURM_ARRAY_TASK_ID}" ;;
+esac
+
+
+#case ${SLURM_ARRAY_TASK_ID} in
+#0) python -u evaluate.py --modelname small --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.01 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
+#1) python -u evaluate.py --modelname small --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.001 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
+#2) python -u evaluate.py --modelname small --dataset spherical_gaussian --algorithm mf --outercouplingmlp --outercouplinglayers 1 --epsilon 0.1 --dropout 0 --dir /scratch/jb6504/manifold-flow ;;
+#*) echo "Nothing to do for job ${SLURM_ARRAY_TASK_ID}" ;;
+#esac
