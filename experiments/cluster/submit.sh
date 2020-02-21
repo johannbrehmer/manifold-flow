546b56ca52754aafd71ae92c7fa01eef79ad8694
Johann Brehmer <mail@johannbrehmer.de>
2020-02-21 16:25:48 -0500

    Prepared re-evaluation of all

--- experiments/cluster/submit.sh
+++ experiments/cluster/submit.sh
@@ -12,55 +12,54 @@
 #sbatch  --array 1-2 train_gamf_lhc.sh
 #
 #sbatch  --array 1-2 train_flow_spherical.sh
-sbatch  --array 0-0 train_pie_spherical.sh
+#sbatch  --array 0-0 train_pie_spherical.sh
 #sbatch  --array 1-2 train_gamf_spherical.sh
 #sbatch  --array 1-2 train_spie_spherical.sh
 #sbatch  --array 1-2 train_sgamf_spherical.sh
 #sbatch  --array 1-2 train_smf_spherical.sh
 #sbatch  --array 1-2 train_pie_epsilon_spherical.sh
-sbatch  --array 21-22 train_mf_spherical.sh
-sbatch  --array 48-49 train_mf_spherical.sh
-sbatch  --array 75-76 train_mf_spherical.sh
+#sbatch  --array 21-22 train_mf_spherical.sh
+#sbatch  --array 48-49 train_mf_spherical.sh
+#sbatch  --array 75-76 train_mf_spherical.sh
 #sbatch  --array 3-26 train_emf_spherical.sh
 #
 #sbatch  --array 1-2 train_flow_csg.sh
-sbatch  --array 0-0 train_pie_csg.sh
+#sbatch  --array 0-0 train_pie_csg.sh
 #sbatch  --array 1-2 train_gamf_csg.sh
 #sbatch  --array 1-2 train_spie_csg.sh
 #sbatch  --array 1-2 train_sgamf_csg.sh
 #sbatch  --array 1-2 train_smf_csg.sh
 #sbatch --array 1-2 train_pie_epsilon_csg.sh
-sbatch  --array 21-22 train_mf_csg.sh
-sbatch  --array 48-49 train_mf_csg.sh
-sbatch  --array 75-76 train_mf_csg.sh
+#sbatch  --array 21-22 train_mf_csg.sh
+#sbatch  --array 48-49 train_mf_csg.sh
+#sbatch  --array 75-76 train_mf_csg.sh
 #sbatch --array 3-26 train_emf_csg.sh
+
 
 # sbatch --array 0-2 evaluate_truth_spherical.sh
 # sbatch --array 0-2 evaluate_truth_csg.sh
 
+sbatch --array 0-2 evaluate_flow_spherical.sh
+sbatch --array 0-2 evaluate_pie_spherical.sh
+sbatch --array 0-35 evaluate_gamf_spherical.sh
+sbatch --array 0-80 evaluate_mf_spherical.sh
+sbatch --array 0-8 evaluate_emf_spherical.sh
+sbatch --array 0-2 evaluate_spie_spherical.sh
+sbatch --array 0-2 evaluate_smf_spherical.sh
+sbatch --array 0-2 evaluate_sgamf_spherical.sh
+sbatch --array 0-2 evaluate_pie_epsilon_spherical.sh
 
-#sbatch --array 1-2 evaluate_flow_spherical.sh
-#sbatch --array 1-2 evaluate_pie_spherical.sh
-#sbatch --array 12-35 evaluate_gamf_spherical.sh
-#sbatch --array 27-80 evaluate_mf_spherical.sh
-#sbatch --array 3-8 evaluate_emf_spherical.sh
-#sbatch --array 1-2 evaluate_spie_spherical.sh
-#sbatch --array 1-2 evaluate_smf_spherical.sh
-#sbatch --array 1-2 evaluate_sgamf_spherical.sh
-#sbatch --array 1-2 evaluate_pie_epsilon_spherical.sh
+sbatch --array 0-2 evaluate_flow_csg.sh
+sbatch --array 0-2 evaluate_pie_csg.sh
+sbatch --array 0-35 evaluate_gamf_csg.sh
+sbatch --array 0-80 evaluate_mf_csg.sh
+sbatch --array 0-8 evaluate_emf_csg.sh
+sbatch --array 0-2 evaluate_spie_csg.sh
+sbatch --array 0-2 evaluate_smf_csg.sh
+sbatch --array 0-2 evaluate_sgamf_csg.sh
+sbatch --array 0-2 evaluate_pie_epsilon_csg.sh
 
-#sbatch --array 1-2 evaluate_flow_csg.sh
-#sbatch --array 1-2 evaluate_pie_csg.sh
-#sbatch --array 12-35 evaluate_gamf_csg.sh
-#sbatch --array 27-80 evaluate_mf_csg.sh
-#sbatch --array 3-8 evaluate_emf_csg.sh
-#sbatch --array 1-2 evaluate_spie_csg.sh
-#sbatch --array 1-2 evaluate_smf_csg.sh
-#sbatch --array 1-2 evaluate_sgamf_csg.sh
-#sbatch --array 1-2 evaluate_pie_epsilon_csg.sh
-
-
-#sbatch --array 0-2 evaluate_flow_lhc2d.sh
-#sbatch --array 0-2 evaluate_flow_lhc.sh
-#sbatch --array 0-2 evaluate_pie_lhc.sh
-#sbatch --array 0-5 evaluate_gamf_lhc.sh
+sbatch --array 0-2 evaluate_flow_lhc2d.sh
+sbatch --array 0-2 evaluate_flow_lhc.sh
+sbatch --array 0-2 evaluate_pie_lhc.sh
+sbatch --array 0-5 evaluate_gamf_lhc.sh
