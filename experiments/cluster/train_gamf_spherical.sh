d88bba9b050bae5b8a2eb6e9a96ffe5445304a15
Johann Brehmer <mail@johannbrehmer.de>
2020-02-21 14:07:13 -0500

    Prepared training shallow long GAMF again

--- experiments/cluster/train_gamf_spherical.sh
+++ experiments/cluster/train_gamf_spherical.sh
@@ -13,18 +13,18 @@ source activate ml
 export OMP_NUM_THREADS=1
 cd /scratch/jb6504/manifold-flow/experiments
 
-python -u train.py --modelname small_hugebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.01 --genbatchsize 5000 --epochs 1000 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
-python -u train.py --modelname small_hugebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.001 --genbatchsize 5000 --epochs 1000 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
-python -u train.py --modelname small_hugebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.1 --genbatchsize 5000 --epochs 1000 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#python -u train.py --modelname small_hugebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.01 --genbatchsize 5000 --epochs 1000 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#python -u train.py --modelname small_hugebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.001 --genbatchsize 5000 --epochs 1000 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#python -u train.py --modelname small_hugebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.1 --genbatchsize 5000 --epochs 1000 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
 
-python -u train.py --modelname small_largebs_shallow_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.01 --samplesize 100000 --genbatchsize 1000 --epochs 500 --outerlayers 3 --innerlayers 3 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#python -u train.py --modelname small_largebs_shallow_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.01 --samplesize 100000 --genbatchsize 1000 --epochs 500 --outerlayers 3 --innerlayers 3 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
 python -u train.py --modelname small_largebs_shallow_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.001 --samplesize 100000 --genbatchsize 1000 --epochs 500 --outerlayers 3 --innerlayers 3 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
-python -u train.py --modelname small_largebs_shallow_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.1 --samplesize 100000 --genbatchsize 1000 --epochs 500 --outerlayers 3 --innerlayers 3 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#python -u train.py --modelname small_largebs_shallow_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.1 --samplesize 100000 --genbatchsize 1000 --epochs 500 --outerlayers 3 --innerlayers 3 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
 
-python -u train.py --modelname small_largebs_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.01 --samplesize 100000 --genbatchsize 1000 --epochs 500 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
-python -u train.py --modelname small_largebs_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.001 --samplesize 100000 --genbatchsize 1000 --epochs 500 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
-python -u train.py --modelname small_largebs_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.1 --samplesize 100000 --genbatchsize 1000 --epochs 500 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
-
-python -u train.py --modelname small_largebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.01 --genbatchsize 1000 --epochs 200 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
-python -u train.py --modelname small_largebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.001 --genbatchsize 1000 --epochs 200 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
-python -u train.py --modelname small_largebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.1 --genbatchsize 1000 --epochs 200 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#python -u train.py --modelname small_largebs_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.01 --samplesize 100000 --genbatchsize 1000 --epochs 500 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#python -u train.py --modelname small_largebs_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.001 --samplesize 100000 --genbatchsize 1000 --epochs 500 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#python -u train.py --modelname small_largebs_long --dataset spherical_gaussian --algorithm gamf --epsilon 0.1 --samplesize 100000 --genbatchsize 1000 --epochs 500 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#
+#python -u train.py --modelname small_largebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.01 --genbatchsize 1000 --epochs 200 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#python -u train.py --modelname small_largebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.001 --genbatchsize 1000 --epochs 200 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
+#python -u train.py --modelname small_largebs --dataset spherical_gaussian --algorithm gamf --epsilon 0.1 --genbatchsize 1000 --epochs 200 --samplesize 100000 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
