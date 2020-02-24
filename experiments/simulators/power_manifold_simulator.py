68a6464d12de2b2dcb90c4e921f442d8c2ed6fab
Johann Brehmer <mail@johannbrehmer.de>
2020-02-24 14:48:10 -0500

    Finished power manifold

--- experiments/simulators/power_manifold_simulator.py
+++ experiments/simulators/power_manifold_simulator.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 
 import numpy as np
-from scipy.stats import norm, uniform
-import itertools
+from scipy.stats import uniform, special_ortho_group
+from sklearn.preprocessing import PolynomialFeatures
 import logging
 from experiments.simulators.base import BaseSimulator
 
@@ -10,20 +10,23 @@ logger = logging.getLogger(__name__)
 
 
 class PowerManifoldSimulator(BaseSimulator):
-    def __init__(self, draw_constants=False, max_power=10, power_decay=1.0, const_width=2.0, min_width=0.1, max_width=1.0, filename=None):
+    def __init__(self, draw_constants=False, max_power=10, power_decay=1.0, weight=0.1, const_width=2.0, min_width=0.1, max_width=1.0, filename=None):
         assert 0.0 < min_width < max_width
         assert power_decay > 0.0
         assert isinstance(max_power, int)
         assert max_power >= 0
+        assert 0. < weight < 1.
 
         self._const_width = const_width
         self._min_width = min_width
         self._max_power = max_power
+        self._weight = weight
         self._power_decay = power_decay
         self._max_width = max_width
 
         if draw_constants:
             self._coeffs, self._rotation = self._draw_constants()
+            self._save_constants(filename)
         else:
             self._coeffs, self._rotation = self._load_constants(filename)
 
@@ -43,13 +46,13 @@ class PowerManifoldSimulator(BaseSimulator):
         assert parameters is not None
 
         z = self._draw_z(n, parameters=parameters)
-        x = self._transform_z_to_x(z_phi, z_eps)
+        x = self._transform_z_to_x(z)
 
         return x
 
     def distance_from_manifold(self, x):
-        z, z_eps = self._transform_x_to_z(x)
-        return np.sum(z_eps ** 2, axis=1) ** 0.5
+        z, eps = self._transform_x_to_z(x)
+        return np.sum(eps ** 2, axis=1) ** 0.5
 
     def sample_from_prior(self, n):
         return uniform.rvs(loc=-1.0, scale=2.0, size=(n, self.parameter_dim()))
@@ -58,97 +61,56 @@ class PowerManifoldSimulator(BaseSimulator):
         parameters = parameters.reshape((-1, self.parameter_dim()))
         return np.sum(uniform.logpdf(parameters, loc=-1.0, scale=2.0), axis=1)
 
-    def _parse_parameters(self, n, parameters):
-        parameters = parameters.reshape(-1, 2)
-        widths_ = np.empty((self._latent_dim, n))
-        phases_ = np.empty((self._latent_dim, n))
-        widths_[:] = self._minwidth + (0.5 + 0.5 * parameters[:, 0]) * (self._maxwidth - self._minwidth)
-        phases_[:-1, :] = 0.5 * np.pi
-        phases_[-1, :] = self._minphase + (0.5 + 0.5 * parameters[:, 1]) * (self._maxphase - self._minphase)
-        widths_ = widths_.T
-        phases_ = phases_.T
+    def _draw_constants(self):
+        n_terms = (self._max_power + 1) * (self._max_power + 2) // 2
+        stddevs = np.ones(n_terms)
+        for i in range(1, self._max_power + 1):
+            stddevs[i * (i + 1) // 2 : (i + 1) * (i + 2) // 2] /= float(i)**(-self._power_decay)
+        logger.debug("Stddevs for coefficients: %s", stddevs)
 
-        return phases_, widths_
+        coeffs = stddevs * np.random.normal(size=n_terms)
+        logger.info("Drew new power coefficients: %s", coeffs)
+
+        rot = special_ortho_group.rvs(3)
+        rot = np.dot(rot, rot)
+        logger.info("Drew new rotation matrix:\n%s", rot)
+
+        return coeffs, rot
+
+    def _load_constants(self, filename):
+        container = np.load(filename)
+        return container["coeffs", "rotation"]
+
+    def _save_constants(self, filename):
+        np.savez(filename, coeffs=self._coeffs, rotation=self._rotation)
 
     def _draw_z(self, n, parameters):
-        # Parameters
-        phases_, widths_ = self._parse_parameters(n, parameters)
+        categories = np.random.choice(2, size=n, replace=True, p=(1.-self._weight, self._weight))
 
-        # Spherical coordinates
-        z_phi = np.random.normal(phases_.flatten(), widths_.flatten(), size=(n * self._latent_dim)).reshape((n, self._latent_dim))
-        z_phi = np.mod(z_phi, 2.0 * np.pi)
+        mean_fix = np.array([[1., -1.]])
+        z_fix = mean_fix + self._const_width * np.random.normal(size=(n * 2)).reshape((n, 2))
 
-        # Fuzzy coordinates
-        z_eps = np.random.normal(0.0, self._epsilon, size=(n * (self._data_dim - self._latent_dim))).reshape((n, self._data_dim - self._latent_dim))
-        return z_phi, z_eps
+        mean_var = np.array([[-1., 1.]])
+        std_var = self._min_width + (self._max_width - self._min_width) * (0.5 + 0.5*parameters.reshape((-1, 1)))
+        z_var = mean_var + std_var * np.random.normal(size=(n * 2)).reshape((n, 2))
 
-    def _transform_z_to_x(self, z_phi, z_eps):
-        r = 1.0 + z_eps[:, 0]
-        a = np.concatenate((2 * np.pi * np.ones((z_phi.shape[0], 1)), z_phi), axis=1)  # n entries, each (2 pi, z_sub)
-        sins = np.sin(a)
-        sins[:, 0] = 1
-        sins = np.cumprod(sins, axis=1)  # n entries, each (1, sin(z0), sin(z1), ..., sin(zk))
-        coss = np.cos(a)
-        coss = np.roll(coss, -1)  # n entries, each (cos(z0), cos(z1), ..., cos(zk), 1)
-        exact_sphere = sins * coss  # (n, k+1)
-        fuzzy_sphere = exact_sphere * r[:, np.newaxis]
-        x = np.concatenate((fuzzy_sphere, z_eps[:, 1:]), axis=1)
+        z = categories * z_var + (1. - categories) * z_fix
+        logger.info("Latent variables:\n%s", z)
+        return z
+
+    def _fz(self, z):
+        powers = PolynomialFeatures(self._max_power, include_bias=True, interaction_only=False).fit_transform(z)
+        fz = powers.dot(self._coeffs).reshape((-1, 1))
+        return fz
+
+    def _transform_z_to_x(self, z):
+        fz = self._fz(z)
+        z_fz = np.concatenate((z, fz), axis=1)
+        x = np.einsum("ij,nj->ni",self._rotation, z_fz)
         return x
 
     def _transform_x_to_z(self, x):
-        z_phi = np.zeros((x.shape[0], self._latent_dim))
-        for i in range(self._latent_dim):
-            z_phi[:, i] = np.arccos(x[:, i] / np.sum(x[:, i : self._latent_dim + 1] ** 2, axis=1) ** 0.5)
-        # Special case for last component, see https://en.wikipedia.org/wiki/N-sphere#Spherical_coordinates
-        z_phi[:, self._latent_dim - 1] = np.where(x[:, self._latent_dim] < 0.0, 2.0 * np.pi - z_phi[:, self._latent_dim - 1], z_phi[:, self._latent_dim - 1])
-
-        r = np.sum(x[:, : self._latent_dim + 1] ** 2, axis=1) ** 0.5
-        z_eps = np.copy(x[:, self._latent_dim :])
-        z_eps[:, 0] = r - 1
-        return z_phi, z_eps
-
-    def _log_density(self, z_phi, z_eps, parameters, precise=False):
-        r = 1.0 + z_eps[:, 0]
-        phases_, widths_ = self._parse_parameters(z_phi.shape[0], parameters)
-
-        p_sub = 0.0
-        for shifted_z_phi in self._generate_equivalent_coordinates(z_phi, precise):
-            p_sub += norm(loc=phases_, scale=widths_).pdf(shifted_z_phi)
-
-        logp_sub = np.log(p_sub)
-        logp_eps = np.log(norm(loc=0.0, scale=self._epsilon).pdf(z_eps))
-
-        log_det = self._latent_dim * np.abs(r)
-        log_det += np.sum(np.arange(self._latent_dim - 1, -1, -1)[np.newaxis, :] * np.log(np.abs(np.sin(z_phi))), axis=1)
-
-        logp = np.concatenate((logp_sub, logp_eps), axis=1)
-        logp = np.sum(logp, axis=1) + log_det
-
-        return logp
-
-    def _generate_equivalent_coordinates(self, z_phi, precise):
-        # Restrict z to canonical range: [0, pi) for polar angles, and [0., 2pi) for azimuthal angle
-        z_phi = z_phi % (2.0 * np.pi)
-        z_phi[:, :-1] = np.where(z_phi[:, :-1] > np.pi, 2.0 * np.pi - z_phi[:, :-1], z_phi[:, :-1])
-
-        # Yield this one
-        yield z_phi
-
-        # Variations of polar angles
-        for dim in range(self._latent_dim - 1):
-            z = np.copy(z_phi)
-            z[:, dim] = -z_phi[:, dim]
-            yield z
-
-            z = np.copy(z_phi)
-            z[:, dim] = 2.0 * np.pi - z_phi[:, dim]
-            yield z
-
-        # Variations of aximuthal angle
-        z = np.copy(z_phi)
-        z[:, -1] = -2.0 * np.pi + z_phi[:, -1]
-        yield z
-
-        z = np.copy(z_phi)
-        z[:, -1] = 2.0 * np.pi + z_phi[:, -1]
-        yield z
+        z_fz = np.einsum("ij,nj->ni",self._rotation.T, x)
+        z = z_fz[:, :2]
+        offset = z_fz[2] - self._fz(z)
+        return z, offset
