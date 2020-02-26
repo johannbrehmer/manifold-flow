7cd91ed86494228786f9db261ab91c97d686882e
Johann Brehmer <mail@johannbrehmer.de>
2020-02-25 19:22:51 -0500

    Updated local test

--- experiments/cluster/test_local1.sh
+++ experiments/cluster/test_local1.sh
@@ -5,4 +5,4 @@ basedir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $basedir/experiments
 
 # python -u train.py --modelname small_alternate --dataset power --algorithm mf --alternate --samplesize 100000 --dir $basedir
-python -u evaluate.py --modelname small_alternate --dataset power --algorithm mf --dir $basedir
+python -u evaluate.py --modelname small_alternate --dataset power --algorithm mf --gridresolution 101 --dir $basedir
