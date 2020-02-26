a8eba9d89bf93f3d091affea4fbc967ec818bd52
Johann Brehmer <mail@johannbrehmer.de>
2020-02-26 09:32:01 -0500

    First alternating results, cleaned up cluster directory

--- experiments/cluster/test_local2.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source activate ml
-basedir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
-cd $basedir/experiments
-
-python -u train.py --modelname small_alternate --dataset power --algorithm gamf --alternate --samplesize 100000 --dir $basedir
