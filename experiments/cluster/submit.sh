b9263df9b92f77fb87c2e4504e30c31cdab29a41
Johann Brehmer <mail@johannbrehmer.de>
2020-02-28 10:32:16 -0500

    Updated submit.sh

--- experiments/cluster/submit.sh
+++ experiments/cluster/submit.sh
@@ -4,21 +4,21 @@
 #sbatch  --array 1-2 simulate.sh
 
 
-#sbatch  --array 0-2 train_flow_lhc2d.sh
-#sbatch  --array 0-2 train_flow_lhc.sh
-#sbatch  --array 0-2 train_pie_lhc.sh
-#sbatch  --array 0-2 train_gamf_lhc.sh
-#sbatch --array 0-2 train_mf_lhc.sh
-#sbatch --array 0-2 train_emf_lhc.sh
-
 #sbatch --array 0-2 train_flow_power.sh
 #sbatch --array 0-2 train_pie_power.sh
 #sbatch --array 0-2 train_gamf_power.sh
 #sbatch --array 0-2 train_pie_epsilon_power.sh
-#sbatch --array 0-2 train_mf_alternating_power.sh
-#sbatch --array 0-2 train_emf_alternating_power.sh
+sbatch --array 0-2 train_mf_alternating_power.sh
+sbatch --array 0-2 train_emf_alternating_power.sh
 #sbatch --array 0-12 train_mf_power.sh
 #sbatch --array 0-12 train_emf_power.sh
+
+#sbatch  --array 0-2 train_flow_lhc2d.sh
+#sbatch  --array 0-2 train_flow_lhc.sh
+#sbatch  --array 0-2 train_pie_lhc.sh
+#sbatch  --array 0-2 train_gamf_lhc.sh
+sbatch --array 0-2 train_mf_lhc.sh
+sbatch --array 0-2 train_emf_lhc.sh
 
 
 #sbatch --array 0-2 evaluate_flow_lhc2d.sh
@@ -29,9 +29,9 @@
 #sbatch --array 0-9 evaluate_gamf_lhc.sh
 
 #sbatch --array 0-2 evaluate_truth_power.sh
-sbatch --array 0-2 evaluate_flow_power.sh
-sbatch --array 0-2 evaluate_pie_power.sh
-sbatch --array 0-2 evaluate_gamf_power.sh
-sbatch --array 0-2 evaluate_pie_epsilon_power.sh
-sbatch --array 0-20 evaluate_mf_power.sh  <--
-sbatch --array 0-20 evaluate_emf_power.sh  <--
+#sbatch --array 0-2 evaluate_flow_power.sh
+#sbatch --array 0-2 evaluate_pie_power.sh
+#sbatch --array 0-2 evaluate_gamf_power.sh
+#sbatch --array 0-2 evaluate_pie_epsilon_power.sh
+#sbatch --array 0-20 evaluate_mf_power.sh  <--
+#sbatch --array 0-20 evaluate_emf_power.sh  <--
