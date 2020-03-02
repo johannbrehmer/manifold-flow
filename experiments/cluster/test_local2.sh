7a6f2057e1dcb7b0709dcfb28a52fb1f9b49b33e
Johann Brehmer <mail@johannbrehmer.de>
2020-03-02 16:32:32 -0500

    New results notebook

--- experiments/cluster/test_local2.sh
+++ experiments/cluster/test_local2.sh
@@ -4,6 +4,13 @@ source activate ml
 basedir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $basedir/experiments
 
-python -u evaluate.py --truth --dataset power --gridresolution 101 --dir $basedir
-python -u evaluate.py --truth --dataset power --gridresolution 101 -i 1 --dir $basedir
-python -u evaluate.py --truth --dataset power --gridresolution 101 -i 2 --dir $basedir
+for SLURM_ARRAY_TASK_ID in 0 1 2
+do
+    python -u evaluate.py --modelname small_shallow_long --dataset power --algorithm flow  --outerlayers 3 --innerlayers 3 --gridresolution 101 -i ${SLURM_ARRAY_TASK_ID} --dir $basedir
+    python -u evaluate.py --modelname small_long --dataset power --algorithm flow  --gridresolution 101 -i ${SLURM_ARRAY_TASK_ID} --dir $basedir
+    python -u evaluate.py --modelname small --dataset power --algorithm flow --gridresolution 101 -i ${SLURM_ARRAY_TASK_ID} --dir $basedir
+
+    python -u evaluate.py --modelname small_shallow_long --dataset power --algorithm pie  --outerlayers 3 --innerlayers 3 --gridresolution 101 -i ${SLURM_ARRAY_TASK_ID} --dir $basedir
+    python -u evaluate.py --modelname small_long --dataset power --algorithm pie  --gridresolution 101 -i ${SLURM_ARRAY_TASK_ID} --dir $basedir
+    python -u evaluate.py --modelname small --dataset power --algorithm pie --gridresolution 101 -i ${SLURM_ARRAY_TASK_ID} --dir $basedir
+done
