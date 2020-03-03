e632c6caa20613305b8a4e39e897dd1ec363498f
Johann Brehmer <mail@johannbrehmer.de>
2020-03-03 09:22:37 -0500

    Updated eval scripts for last (?) missing power jobs

--- experiments/cluster/evaluate_gamf_lhc.sh
+++ experiments/cluster/evaluate_gamf_lhc.sh
@@ -12,13 +12,17 @@ source activate ml
 export OMP_NUM_THREADS=1
 cd /scratch/jb6504/manifold-flow/experiments
 
-run=$((task / 3))
-task=$((task % 3))
+run=$((task / 6))
+task=$((task % 6))
 echo "SLURM_ARRAY_TASK_ID = ${SLURM_ARRAY_TASK_ID}, task = ${task}, run = ${run}"
 
 case ${task} in
-0) python -u evaluate.py --modelname largebs --dataset lhc --algorithm gamf --modellatentdim 9  --observedsamples 100 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
-1) python -u evaluate.py --modelname hugebs --dataset lhc --algorithm gamf --modellatentdim 9  --observedsamples 100 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
-2) python -u evaluate.py --modelname alternate_largebs --dataset lhc --algorithm gamf --modellatentdim 9  --observedsamples 100 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+0) python -u evaluate.py --modelname wdecay_largebs --dataset lhc --algorithm gamf --modellatentdim 9  --observedsamples 100 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+1) python -u evaluate.py --modelname wdecay_hugebs --dataset lhc --algorithm gamf --modellatentdim 9  --observedsamples 100 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+2) python -u evaluate.py --modelname largebs --dataset lhc --algorithm gamf --modellatentdim 9  --observedsamples 100 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+3) python -u evaluate.py --modelname hugebs --dataset lhc --algorithm gamf --modellatentdim 9  --observedsamples 100 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+
+4) python -u evaluate.py --modelname wdecay_alternate_largebs --dataset lhc --algorithm gamf --modellatentdim 9  --observedsamples 100 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+5) python -u evaluate.py --modelname alternate_largebs --dataset lhc --algorithm gamf --modellatentdim 9  --observedsamples 100 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
 *) echo "Nothing to do for job ${task}" ;;
 esac
--- experiments/cluster/evaluate_gamf_power.sh
+++ experiments/cluster/evaluate_gamf_power.sh
@@ -12,14 +12,27 @@ source activate ml
 export OMP_NUM_THREADS=1
 cd /scratch/jb6504/manifold-flow/experiments
 
-run=$((SLURM_ARRAY_TASK_ID / 4))
-task=$((SLURM_ARRAY_TASK_ID % 4))
+run=$((SLURM_ARRAY_TASK_ID / 14))
+task=$((SLURM_ARRAY_TASK_ID % 14))
 echo "SLURM_ARRAY_TASK_ID = ${SLURM_ARRAY_TASK_ID}, task = ${task}, run = ${run}"
 
 case ${task} in
-0) python -u evaluate.py --modelname small_largebs --dataset power --algorithm gamf --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
-1) python -u evaluate.py --modelname small_hugebs --dataset power --algorithm gamf  --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
-2) python -u evaluate.py --modelname small_largebs_shallow_long --dataset power --algorithm gamf   --outerlayers 3 --innerlayers 3 --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
-3) python -u evaluate.py --modelname small_largebs_long --dataset power --algorithm gamf   --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+0) python -u evaluate.py --modelname small_wdecay_largebs --dataset power --algorithm gamf --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+1) python -u evaluate.py --modelname small_wdecay_hugebs --dataset power --algorithm gamf  --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+2) python -u evaluate.py --modelname small_wdecay_largebs_shallow_long --dataset power --algorithm gamf   --outerlayers 3 --innerlayers 3 --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+3) python -u evaluate.py --modelname small_wdecay_largebs_long --dataset power --algorithm gamf   --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+
+4) python -u evaluate.py --modelname small_largebs --dataset power --algorithm gamf --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+5) python -u evaluate.py --modelname small_hugebs --dataset power --algorithm gamf  --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+6) python -u evaluate.py --modelname small_largebs_shallow_long --dataset power --algorithm gamf   --outerlayers 3 --innerlayers 3 --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+7) python -u evaluate.py --modelname small_largebs_long --dataset power --algorithm gamf   --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+
+8) python -u evaluate.py --modelname small_alternate_wdecay --dataset power --algorithm gamf  --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+9) python -u evaluate.py --modelname small_alternate_wdecay_shallow_long --dataset power --algorithm gamf   --outerlayers 3 --innerlayers 3 --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+10) python -u evaluate.py --modelname small_alternate_wdecay_long --dataset power --algorithm gamf   --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+
+11) python -u evaluate.py --modelname small_alternate --dataset power --algorithm gamf  --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+12) python -u evaluate.py --modelname small_alternate_shallow_long --dataset power --algorithm gamf   --outerlayers 3 --innerlayers 3 --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
+13) python -u evaluate.py --modelname small_alternate_long --dataset power --algorithm gamf   --gridresolution 101 -i ${run} --dir /scratch/jb6504/manifold-flow ;;
 *) echo "Nothing to do for job ${task}" ;;
 esac
