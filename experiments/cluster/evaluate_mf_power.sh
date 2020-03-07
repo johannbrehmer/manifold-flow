300c0c74004d5ff8015a688fb4e372e110ec0de8
Johann <mail@johannbrehmer.de>
2020-03-07 14:42:45 -0500

    Restarted alternating generative runs for LHC

--- experiments/cluster/evaluate_mf_power.sh
+++ experiments/cluster/evaluate_mf_power.sh
@@ -18,6 +18,6 @@ echo "SLURM_ARRAY_TASK_ID = ${SLURM_ARRAY_TASK_ID}, task = ${task}, run = ${run}
 
 case ${task} in
 0) python -u evaluate.py --modelname alternate_march --dataset power --algorithm mf --splinebins 10 --splinerange 6. --gridresolution 101 -i ${run} ;;
-0) python -u evaluate.py --modelname march --dataset power --algorithm mf --splinebins 10 --splinerange 6. --gridresolution 101 -i ${run} ;;
+1) python -u evaluate.py --modelname march --dataset power --algorithm mf --splinebins 10 --splinerange 6. --gridresolution 101 -i ${run} ;;
 *) echo "Nothing to do for job ${SLURM_ARRAY_TASK_ID}" ;;
 esac
