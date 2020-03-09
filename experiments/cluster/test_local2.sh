cd2c59d5cf5dcb9236a4fccbca77429c25b718a9
Johann Brehmer <mail@johannbrehmer.de>
2020-03-09 16:54:57 -0400

    Local eval

--- experiments/cluster/test_local2.sh
+++ experiments/cluster/test_local2.sh
@@ -4,4 +4,9 @@ source activate ml
 basedir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $basedir/experiments
 
-python -u evaluate.py --modelname march --dataset lhc --algorithm flow --modellatentdim 14 --splinebins 10 --observedsamples 100 -i 0 --dir $basedir
+for i in 0 1 2
+do
+    python -u evaluate.py --modelname march --dataset lhc2d --algorithm flow --modellatentdim 2 --observedsamples 100 -i $i --dir $basedir
+    python -u evaluate.py --modelname march --dataset lhc --algorithm pie --modellatentdim 14 --splinebins 10 --observedsamples 100 -i $i --dir $basedir
+    python -u evaluate.py --modelname march --dataset lhc --algorithm flow --modellatentdim 14 --splinebins 10 --observedsamples 100 -i $i --dir $basedir
+done
