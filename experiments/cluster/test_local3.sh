693c26fffccff12519d26496d3b6f23c4b828d77
Johann Brehmer <mail@johannbrehmer.de>
2020-03-09 19:13:26 -0400

    Sneak at MLF results

--- experiments/cluster/test_local3.sh
+++ experiments/cluster/test_local3.sh
@@ -4,4 +4,4 @@ source activate ml
 basedir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $basedir/experiments
 
-python -u evaluate.py --modelname alternate_march --dataset lhc --algorithm mf --modellatentdim 14 --splinebins 10 --observedsamples 100 --mcmcsamples 100 -i 0 --dir $basedir
+python -u evaluate.py --modelname alternate_march --dataset lhc --algorithm mf --modellatentdim 14 --splinebins 10 --observedsamples 100 --mcmcsamples 100 -i 2 --dir $basedir
