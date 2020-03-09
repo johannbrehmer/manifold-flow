b1deb95fef2bc58647a60344da916fb08c5a9229
Johann Brehmer <mail@johannbrehmer.de>
2020-03-09 19:07:11 -0400

    Fixed LHC PIE training script

--- experiments/cluster/test_local2.sh
+++ experiments/cluster/test_local2.sh
@@ -6,7 +6,7 @@ cd $basedir/experiments
 
 for i in 0 1 2
 do
-    python -u evaluate.py --modelname march --dataset lhc2d --algorithm flow --modellatentdim 2 --observedsamples 100 -i $i --dir $basedir
+    # python -u evaluate.py --modelname march --dataset lhc2d --algorithm flow --modellatentdim 2 --observedsamples 100 -i $i --dir $basedir
     python -u evaluate.py --modelname march --dataset lhc --algorithm pie --modellatentdim 14 --splinebins 10 --observedsamples 100 -i $i --dir $basedir
-    python -u evaluate.py --modelname march --dataset lhc --algorithm flow --modellatentdim 14 --splinebins 10 --observedsamples 100 -i $i --dir $basedir
+    # python -u evaluate.py --modelname march --dataset lhc --algorithm flow --modellatentdim 14 --splinebins 10 --observedsamples 100 -i $i --dir $basedir
 done
