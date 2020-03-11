19ddd3223cbd1e067cc1aaa90ae99b50b0c2bd6b
Johann Brehmer <mail@johannbrehmer.de>
2020-03-11 11:12:28 -0400

    Prepared running a 4th and 5th experiment run

--- experiments/cluster/submit.sh
+++ experiments/cluster/submit.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 
-#sbatch  --array 0-2 simulate.sh
+# sbatch --array 3-9 simulate.sh
 
 
-#sbatch --array 0-2 train_flow_power.sh
-#sbatch --array 0-2 train_pie_power.sh
-#sbatch --array 0-2 train_gamf_power.sh
-#sbatch --array 0-5 train_mf_power.sh
-#sbatch --array 0-5 train_emf_power.sh
+sbatch --array 3-4 train_flow_power.sh
+sbatch --array 3-4 train_pie_power.sh
+sbatch --array 3-4 train_gamf_power.sh
+sbatch --array 6-9 train_mf_power.sh
+sbatch --array 6-9 train_emf_power.sh
 
-#sbatch --array 0-2 train_flow_lhc2d.sh
-#sbatch --array 0-2 train_flow_lhc.sh
-#sbatch --array 0-2 train_pie_lhc.sh
-#sbatch --array 0-2 train_gamf_lhc.sh
-#sbatch --array 0-2 train_mf_lhc.sh
-#sbatch --array 0-2 train_emf_lhc.sh
+sbatch --array 3-4 train_flow_lhc2d.sh
+sbatch --array 3-4 train_flow_lhc.sh
+sbatch --array 3-4 train_pie_lhc.sh
+sbatch --array 3-4 train_gamf_lhc.sh
+sbatch --array 3-4 train_mf_lhc.sh
+sbatch --array 3-4 train_emf_lhc.sh
 
 
 #sbatch --array 0-2 evaluate_truth_power.sh
 #sbatch --array 0-2 evaluate_flow_power.sh
 #sbatch --array 0-2 evaluate_pie_power.sh
 #sbatch --array 0-6 evaluate_gamf_power.sh
-sbatch --array 1-1 evaluate_mf_power.sh
-sbatch --array 3-5 evaluate_mf_power.sh
-sbatch --array 0-5 evaluate_emf_power.sh
+#sbatch --array 0-2 evaluate_mf_power.sh
+#sbatch --array 0-5 evaluate_mf_power.sh
+#sbatch --array 0-5 evaluate_emf_power.sh
 
 #sbatch --array 0-2 evaluate_flow_lhc2d.sh
 #sbatch --array 0-2 evaluate_flow_lhc.sh
--- experiments/generate_data.py
+++ experiments/generate_data.py
@@ -50,12 +50,12 @@ if __name__ == "__main__":
     parameters_train = simulator.sample_from_prior(args.train) if conditional else None
 
     # Sample
-    # if args.train > 0:
-    #     logger.info("Generating %s training samples at parameters %s", args.train, parameters_train)
-    #     x_train = simulator.sample(args.train, parameters=parameters_train)
-    #     np.save(create_filename("sample", "x_train", args), x_train)
-    #     if conditional:
-    #         np.save(create_filename("sample", "parameters_train", args), parameters_train)
+    if args.train > 0:
+        logger.info("Generating %s training samples at parameters %s", args.train, parameters_train)
+        x_train = simulator.sample(args.train, parameters=parameters_train)
+        np.save(create_filename("sample", "x_train", args), x_train)
+        if conditional:
+            np.save(create_filename("sample", "parameters_train", args), parameters_train)
 
     if args.paramscan > 0:
         parameters_val = np.array([simulator.default_parameters() for _ in range(args.paramscan)]).reshape((args.paramscan, -1)) if conditional else None
@@ -65,19 +65,19 @@ if __name__ == "__main__":
         if conditional:
             np.save(create_filename("sample", "parameters_paramscan", args), parameters_val)
 
-    # if args.test > 0:
-    #     parameters_test = np.array([simulator.default_parameters() for _ in range(args.test)]).reshape((args.test, -1)) if conditional else None
-    #     logger.info("Generating %s test samples at parameters %s", args.test, parameters_test)
-    #     x_test = simulator.sample(args.test, parameters=parameters_test)
-    #     np.save(create_filename("sample", "x_test", args), x_test)
-    #     if conditional:
-    #         np.save(create_filename("sample", "parameters_test", args), parameters_test)
-    #
-    # if args.ood > 0:
-    #     logger.info("Generating %s ood samples at parameters %s", args.ood, parameters_test)
-    #     x_ood = simulator.sample_ood(args.ood, parameters=parameters_test)
-    #     np.save(create_filename("sample", "x_ood", args), x_ood)
-    #     if conditional:
-    #         np.save(create_filename("sample", "parameters_ood", args), parameters_test)
+    if args.test > 0:
+        parameters_test = np.array([simulator.default_parameters() for _ in range(args.test)]).reshape((args.test, -1)) if conditional else None
+        logger.info("Generating %s test samples at parameters %s", args.test, parameters_test)
+        x_test = simulator.sample(args.test, parameters=parameters_test)
+        np.save(create_filename("sample", "x_test", args), x_test)
+        if conditional:
+            np.save(create_filename("sample", "parameters_test", args), parameters_test)
+
+    if args.ood > 0:
+        logger.info("Generating %s ood samples at parameters %s", args.ood, parameters_test)
+        x_ood = simulator.sample_ood(args.ood, parameters=parameters_test)
+        np.save(create_filename("sample", "x_ood", args), x_ood)
+        if conditional:
+            np.save(create_filename("sample", "parameters_ood", args), parameters_test)
 
     logger.info("All done! Have a nice day!")
