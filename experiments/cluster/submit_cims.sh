bfa86ed725096f19b88a6fc02ae126a04b471802
Johann Brehmer <mail@johannbrehmer.de>
2020-03-11 14:40:29 -0400

    Fixed submit_cims.sh

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -3,6 +3,6 @@
 sbatch --array 1-2 evaluate_flow_lhc2d_cims.sh
 sbatch --array 1-2 evaluate_flow_lhc_cims.sh
 sbatch --array 1-2 evaluate_pie_lhc_cims.sh
-sbatch --array 1-2 evaluate_mf_lhc_cims.sh
+sbatch --array 1-1 evaluate_mf_lhc_cims.sh
 sbatch --array 1-2 evaluate_emf_lhc_cims.sh
 sbatch --array 2-5 evaluate_gamf_lhc_cims.sh
