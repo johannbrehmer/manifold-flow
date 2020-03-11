08a2a5ed49763c6f0fe5cccd2d827b9852f441b6
Johann Brehmer <mail@johannbrehmer.de>
2020-03-11 11:22:49 -0400

    Cleaned up train scripts

--- experiments/cluster/train_flow_lhc.sh
+++ experiments/cluster/train_flow_lhc.sh
@@ -14,5 +14,3 @@ export OMP_NUM_THREADS=1
 cd /scratch/jb6504/manifold-flow/experiments
 
 python -u train.py --modelname march --dataset lhc --algorithm flow --modellatentdim 14 --splinebins 10 -i ${SLURM_ARRAY_TASK_ID}
-
-#python -u train.py --dataset lhc --algorithm flow --modellatentdim 9 --epochs 100 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
