a2302cf39a2f321051c26497f86ef3eb5c9b48a3
Johann Brehmer <brehmer@cassio.cs.nyu.edu>
2020-03-11 10:44:46 -0400

    CIMS conda fun

--- experiments/cluster/evaluate_flow_lhc_cims.sh
+++ experiments/cluster/evaluate_flow_lhc_cims.sh
@@ -8,10 +8,16 @@
 #SBATCH --time=2-00:00:00
 # #SBATCH --gres=gpu:1
 
-source /home/brehmer/miniconda3/etc/profile.d/conda.sh
+# source /home/brehmer/.bashrc
+# source /home/brehmer/miniconda3/etc/profile.d/conda.sh
+
 conda activate ml
+export PATH="/home/brehmer/miniconda3/envs/ml/bin/:$PATH"
 export OMP_NUM_THREADS=1
 dir=/home/brehmer/manifold-flow
+
+conda list
+which python
 
 cd $dir/experiments
 python -u evaluate.py --modelname march --dataset lhc --algorithm flow --modellatentdim 14 --splinebins 10 --observedsamples 100 -i ${SLURM_ARRAY_TASK_ID} --dir $dir
