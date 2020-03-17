921a0cfb1e04083a61ab355a4f3d733461c3d929
Johann Brehmer <mail@johannbrehmer.de>
2020-03-17 18:23:45 -0400

    Updated some more things for the April runs

--- experiments/cluster/train_flow_lhc2d.sh
+++ experiments/cluster/train_flow_lhc2d.sh
@@ -13,4 +13,4 @@ source activate ml
 export OMP_NUM_THREADS=1
 cd /scratch/jb6504/manifold-flow/experiments
 
-python -u train.py --modelname april --dataset lhc2d --algorithm flow --splinebins 10 --nllfactor 0.1 --sinkhornfactor 1 --modellatentdim 2 -i ${SLURM_ARRAY_TASK_ID}
+python -u train.py --modelname april --dataset lhc2d --algorithm flow --splinebins 10 --nllfactor 1 --modellatentdim 2 -i ${SLURM_ARRAY_TASK_ID}
--- experiments/cluster/train_gamf_lhc.sh
+++ experiments/cluster/train_gamf_lhc.sh
@@ -13,5 +13,5 @@ source activate ml
 export OMP_NUM_THREADS=1
 cd /scratch/jb6504/manifold-flow/experiments
 
-python -u train.py --modelname april --dataset lhc --algorithm gamf --modellatentdim 14 --splinebins 10 --nllfactor 0.1 --sinkhornfactor 1 -i ${SLURM_ARRAY_TASK_ID}
+python -u train.py --modelname april --dataset lhc --algorithm gamf --modellatentdim 14 --splinebins 10 --sinkhornfactor 1 -i ${SLURM_ARRAY_TASK_ID}
 python -u train.py --modelname alternate_april --dataset lhc --algorithm gamf --alternate --modellatentdim 14 --splinebins 10 --nllfactor 0.1 --sinkhornfactor 1 --subsets 100 -i ${SLURM_ARRAY_TASK_ID}
