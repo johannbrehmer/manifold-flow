65cb605fc19c4231b3d8186bd0f48ac5b6e8fc6a
Johann Brehmer <mail@johannbrehmer.de>
2020-03-20 11:19:29 -0400

    Prepared additional training on CIMS

---
+++ experiments/cluster/train_emf_lhc_cims.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=t-emf-l
+#SBATCH --output=log_train_emf_lhc_%a.log
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32GB
+#SBATCH --time=7-00:00:00
+#SBATCH --gres=gpu:1
+
+conda activate ml
+export PATH="/home/brehmer/miniconda3/envs/ml/bin/:$PATH"
+export OMP_NUM_THREADS=1
+dir=/home/brehmer/manifold-flow
+cd $dir/experiments
+
+python -u train.py --modelname alternate_april --dataset lhc --algorithm emf --alternate --modellatentdim 14 --splinebins 10 --nllfactor 0.1 --sinkhornfactor 1 --subsets 100 -i ${SLURM_ARRAY_TASK_ID}
