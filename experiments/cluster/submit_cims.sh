0289fb27b468e83d2cc98b3bb9d23c077cba4f5c
Johann Brehmer <mail@johannbrehmer.de>
2020-03-21 18:49:06 -0400

    Prepared next set of eval runs on CIMS

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
 # Run 1
-sbatch --array 0-11 evaluate_flow_lhc2d_cims.sh
-sbatch --array 0-11 evaluate_flow_lhc_cims.sh
-sbatch --array 0-11 evaluate_pie_lhc_cims.sh
-#sbatch --array 0-11 evaluate_mf_lhc_cims.sh
+# sbatch --array 0-11 evaluate_flow_lhc2d_cims.sh  # check
+# sbatch --array 0-11 evaluate_flow_lhc_cims.sh  # check
+# sbatch --array 0-11 evaluate_pie_lhc_cims.sh  # check
+sbatch --array 0-11 evaluate_mf_lhc_cims.sh
 #sbatch --array 0-11 evaluate_emf_lhc_cims.sh
 #sbatch --array 0-11 evaluate_gamf_lhc_cims.sh
 #sbatch --array 0-11 evaluate_gamf2_lhc_cims.sh
 
-sleep 60
+# sleep 60
 
 # Run 2-3
-sbatch --array 12-35 evaluate_flow_lhc2d_cims.sh
-sbatch --array 12-35 evaluate_flow_lhc_cims.sh
-#sbatch --array 12-35 evaluate_pie_lhc_cims.sh
-#sbatch --array 12-35 evaluate_mf_lhc_cims.sh
-#sbatch --array 12-35 evaluate_emf_lhc_cims.sh
-#sbatch --array 12-35 evaluate_gamf_lhc_cims.sh
-#sbatch --array 12-35 evaluate_gamf2_lhc_cims.sh
+# sbatch --array 12-35 evaluate_flow_lhc2d_cims.sh  # check
+# sbatch --array 12-35 evaluate_flow_lhc_cims.sh  # check
+sbatch --array 12-35 evaluate_pie_lhc_cims.sh
+# sbatch --array 12-35 evaluate_mf_lhc_cims.sh
+# sbatch --array 12-23 evaluate_emf_lhc_cims.sh
+sbatch --array 24-35 evaluate_emf_lhc_cims.sh
+# sbatch --array 12-35 evaluate_gamf_lhc_cims.sh
+# sbatch --array 12-35 evaluate_gamf2_lhc_cims.sh
 
 # sleep 60
 
 # Run 4-5
-#sbatch --array 36-59 evaluate_flow_lhc2d_cims.sh
-#sbatch --array 36-59 evaluate_flow_lhc_cims.sh
-#sbatch --array 36-59 evaluate_pie_lhc_cims.sh
-#sbatch --array 36-59 evaluate_mf_lhc_cims.sh
-#sbatch --array 36-59 evaluate_emf_lhc_cims.sh
-#sbatch --array 36-59 evaluate_gamf_lhc_cims.sh
-#sbatch --array 36-59 evaluate_gamf2_lhc_cims.sh
+sbatch --array 36-59 evaluate_flow_lhc2d_cims.sh
+sbatch --array 36-59 evaluate_flow_lhc_cims.sh
+# sbatch --array 36-59 evaluate_pie_lhc_cims.sh
+# sbatch --array 36-59 evaluate_mf_lhc_cims.sh
+# sbatch --array 36-59 evaluate_emf_lhc_cims.sh
+# sbatch --array 36-59 evaluate_gamf_lhc_cims.sh
+# sbatch --array 36-59 evaluate_gamf2_lhc_cims.sh
