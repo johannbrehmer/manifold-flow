6c428e98debc188daf3eb0b0ea0f0d63dc2cbff0
Johann Brehmer <mail@johannbrehmer.de>
2020-03-22 15:42:47 -0400

    Updated submission scripts for next batch of runs

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -5,7 +5,7 @@
 # sbatch --array 0-11 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 0-11 evaluate_pie_lhc_cims.sh  # done
 # sbatch --array 0-11 evaluate_mf_lhc_cims.sh  # submitted
-sbatch --array 0-11 evaluate_emf_lhc_cims.sh
+# sbatch --array 0-11 evaluate_emf_lhc_cims.sh  # submitted
 #sbatch --array 0-11 evaluate_gamf_lhc_cims.sh
 #sbatch --array 0-11 evaluate_gamf2_lhc_cims.sh
 
@@ -15,8 +15,8 @@ sbatch --array 0-11 evaluate_emf_lhc_cims.sh
 # sbatch --array 12-35 evaluate_flow_lhc2d_cims.sh  # done
 # sbatch --array 12-35 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 12-35 evaluate_pie_lhc_cims.sh  # submitted
-sbatch --array 12-35 evaluate_mf_lhc_cims.sh
-sbatch --array 12-23 evaluate_emf_lhc_cims.sh
+# sbatch --array 12-35 evaluate_mf_lhc_cims.sh  # submitted
+# sbatch --array 12-23 evaluate_emf_lhc_cims.sh  # submitted
 # sbatch --array 24-35 evaluate_emf_lhc_cims.sh  # submitted
 # sbatch --array 12-35 evaluate_gamf_lhc_cims.sh
 # sbatch --array 12-35 evaluate_gamf2_lhc_cims.sh
@@ -26,7 +26,7 @@ sbatch --array 12-23 evaluate_emf_lhc_cims.sh
 # Run 4-5
 # sbatch --array 36-59 evaluate_flow_lhc2d_cims.sh  # submitted
 # sbatch --array 36-59 evaluate_flow_lhc_cims.sh  # submitted
-sbatch --array 36-59 evaluate_pie_lhc_cims.sh
+# sbatch --array 36-59 evaluate_pie_lhc_cims.sh  # submitted
 sbatch --array 36-59 evaluate_mf_lhc_cims.sh
 sbatch --array 36-59 evaluate_emf_lhc_cims.sh
 # sbatch --array 36-59 evaluate_gamf_lhc_cims.sh
