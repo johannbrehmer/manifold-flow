3f4722b0e51f612b7dd56c7027ac48895e545aaa
Johann Brehmer <mail@johannbrehmer.de>
2020-03-26 14:44:14 -0400

    Result gazing

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -7,20 +7,20 @@
 
 # sbatch --array 0-11 evaluate_flow_lhc2d_cims.sh  # done
 
-sbatch --array 0-11 evaluate_flow_lhc40d_cims.sh
-sbatch --array 0-11 evaluate_pie_lhc40d_cims.sh
-sbatch --array 0-11 evaluate_mf_lhc40d_cims.sh
-sbatch --array 0-11 evaluate_emf_lhc40d_cims.sh
+# sbatch --array 0-11 evaluate_flow_lhc40d_cims.sh
+# sbatch --array 0-11 evaluate_pie_lhc40d_cims.sh
+# sbatch --array 0-11 evaluate_mf_lhc40d_cims.sh
+# sbatch --array 0-11 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 0-11 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 0-11 evaluate_pie_lhc_cims.sh  # done
 # sbatch --array 0-11 evaluate_mf_lhc_cims.sh  # done
 # sbatch --array 0-11 evaluate_emf_lhc_cims.sh  # done
 
-sbatch --array 0-11 evaluate_flow_lhc_long_cims.sh
-sbatch --array 0-11 evaluate_pie_lhc_long_cims.sh
-sbatch --array 0-11 evaluate_mf_lhc_long_cims.sh
-sbatch --array 0-11 evaluate_emf_lhc_long_cims.sh
+# sbatch --array 0-11 evaluate_flow_lhc_long_cims.sh
+# sbatch --array 0-11 evaluate_pie_lhc_long_cims.sh
+# sbatch --array 0-11 evaluate_mf_lhc_long_cims.sh
+# sbatch --array 0-11 evaluate_emf_lhc_long_cims.sh
 
 
 sleep 60
@@ -32,18 +32,18 @@ sleep 60
 
 # sbatch --array 12-35 evaluate_flow_lhc2d_cims.sh  # done
 
-sbatch --array 12-35 evaluate_flow_lhc40d_cims.sh
-sbatch --array 12-35 evaluate_pie_lhc40d_cims.sh
-sbatch --array 12-35 evaluate_mf_lhc40d_cims.sh
-sbatch --array 12-35 evaluate_emf_lhc40d_cims.sh
+# sbatch --array 12-35 evaluate_flow_lhc40d_cims.sh
+# sbatch --array 12-35 evaluate_pie_lhc40d_cims.sh
+# sbatch --array 12-35 evaluate_mf_lhc40d_cims.sh
+# sbatch --array 12-35 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 12-35 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 12-35 evaluate_pie_lhc_cims.sh  # done
 # sbatch --array 12-35 evaluate_mf_lhc_cims.sh  # done
 # sbatch --array 12-35 evaluate_emf_lhc_cims.sh  # done
 
-sbatch --array 12-35 evaluate_flow_lhc_long_cims.sh
-sbatch --array 12-35 evaluate_pie_lhc_long_cims.sh
+# sbatch --array 12-35 evaluate_flow_lhc_long_cims.sh
+# sbatch --array 12-35 evaluate_pie_lhc_long_cims.sh
 # sbatch --array 12-35 evaluate_mf_lhc_long_cims.sh
 # sbatch --array 12-35 evaluate_emf_lhc_long_cims.sh
 
@@ -65,8 +65,8 @@ sbatch --array 12-35 evaluate_pie_lhc_long_cims.sh
 # sbatch --array 36-59 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 36-59 evaluate_pie_lhc_cims.sh  # done
 # sbatch --array 36-59 evaluate_mf_lhc_cims.sh  # done
-# sbatch --array 36-47 evaluate_emf_lhc_cims.sh  # done
-# sbatch --array 48-59 evaluate_emf_lhc_cims.sh
+# sbatch --array 36-47 evaluate_emf_lhc_cims.sh
+sbatch --array 48-59 evaluate_emf_lhc_cims.sh
 
 # sbatch --array 36-59 evaluate_flow_lhc_long_cims.sh
 # sbatch --array 36-59 evaluate_pie_lhc_long_cims.sh
