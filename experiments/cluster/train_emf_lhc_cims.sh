6b996b995f1362d57187324abf28231357aa10ef
Johann Brehmer <mail@johannbrehmer.de>
2020-03-27 12:03:24 -0400

    Go CIMS

--- experiments/cluster/train_emf_lhc_cims.sh
+++ experiments/cluster/train_emf_lhc_cims.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32GB
 #SBATCH --time=2-00:00:00
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:1080ti:1
 
 conda activate ml
 export PATH="/home/brehmer/miniconda3/envs/ml/bin/:$PATH"
@@ -14,4 +14,4 @@ export OMP_NUM_THREADS=1
 dir=/home/brehmer/manifold-flow
 cd $dir/experiments
 
-python -u train.py --modelname alternate_april --dataset lhc --algorithm emf --alternate --modellatentdim 14 --splinebins 10 --nllfactor 0.1 --sinkhornfactor 1 --subsets 100 -i ${SLURM_ARRAY_TASK_ID} --dir $dir
+python -u train.py --modelname alternate_april3 --dataset lhc --algorithm emf --alternate --modellatentdim 14 --splinebins 10 --nllfactor 0.1 --sinkhornfactor 1 --subsets 100 -i ${SLURM_ARRAY_TASK_ID} --dir $dir
