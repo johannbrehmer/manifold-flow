39ba50f9b422f33e8928e4f462f3d488204852a5
Johann Brehmer <mail@johannbrehmer.de>
2020-03-27 14:43:02 -0400

    Prepared local eval for mixture model

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -10,7 +10,7 @@
 # sbatch --array 0-11 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 0-11 evaluate_pie_lhc40d_cims.sh  # done
 # sbatch --array 0-11 evaluate_mf_lhc40d_cims.sh
-# sbatch --array 0-11 evaluate_emf_lhc40d_cims.sh  # submitted
+# sbatch --array 0-11 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 0-11 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 0-11 evaluate_pie_lhc_cims.sh  # done
@@ -35,7 +35,7 @@
 # sbatch --array 12-35 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 12-35 evaluate_pie_lhc40d_cims.sh  # done
 # sbatch --array 12-35 evaluate_mf_lhc40d_cims.sh
-# sbatch --array 12-23 evaluate_emf_lhc40d_cims.sh  # submitted
+# sbatch --array 12-23 evaluate_emf_lhc40d_cims.sh
 # sbatch --array 24-35 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 12-35 evaluate_flow_lhc_cims.sh  # done
@@ -61,14 +61,14 @@
 # sbatch --array 36-59 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 36-59 evaluate_pie_lhc40d_cims.sh  # done
 # sbatch --array 36-59 evaluate_mf_lhc40d_cims.sh
-# sbatch --array 36-47 evaluate_emf_lhc40d_cims.sh  # submitted
+# sbatch --array 36-47 evaluate_emf_lhc40d_cims.sh
 # sbatch --array 48-59 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 36-59 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 36-59 evaluate_pie_lhc_cims.sh  # done
 # sbatch --array 36-59 evaluate_mf_lhc_cims.sh  # done
-# sbatch --array 36-47 evaluate_emf_lhc_cims.sh  # submitted
-# sbatch --array 48-59 evaluate_emf_lhc_cims.sh  # done
+# sbatch --array 36-47 evaluate_emf_lhc_cims.sh
+# sbatch --array 48-59 evaluate_emf_lhc_cims.sh
 
 # sbatch --array 36-59 evaluate_flow_lhc_long_cims.sh
 # sbatch --array 36-59 evaluate_pie_lhc_long_cims.sh
---
+++ experiments/cluster/test_local3.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source activate ml
+dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
+cd $dir/experiments
+
+python -u evaluate.py --modelname alternate_march --dataset power --algorithm emf --splinebins 10 --splinerange 6. --gridresolution 101 -i 1 --dir $dir
