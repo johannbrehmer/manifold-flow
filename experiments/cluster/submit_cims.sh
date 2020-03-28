19c0f805e1abc6786f7db9ba83d0b21413034d71
Johann Brehmer <mail@johannbrehmer.de>
2020-03-28 17:32:25 -0400

    Prepared everything for Saturday night CIMS jobs

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -19,7 +19,7 @@
 
 # sbatch --array 0-11 evaluate_flow_lhc_long_cims.sh  # done
 # sbatch --array 0-11 evaluate_pie_lhc_long_cims.sh  # done
-sbatch --array 0-11 evaluate_mf_lhc_long_cims.sh
+# sbatch --array 0-11 evaluate_mf_lhc_long_cims.sh  # submitted
 # sbatch --array 0-11 evaluate_emf_lhc_long_cims.sh
 
 
@@ -59,7 +59,7 @@ sbatch --array 0-11 evaluate_mf_lhc_long_cims.sh
 
 # sbatch --array 36-59 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 36-59 evaluate_pie_lhc40d_cims.sh  # done
-# sbatch --array 36-59 evaluate_mf_lhc40d_cims.sh
+sbatch --array 36-59 evaluate_mf_lhc40d_cims.sh
 # sbatch --array 36-59 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 36-59 evaluate_flow_lhc_cims.sh  # done
