4e039ac81125e00b2db3cd1618ace9edde43b19d
Johann Brehmer <mail@johannbrehmer.de>
2020-03-28 07:47:22 -0400

    Prepared next CIMS eval runs

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -9,7 +9,7 @@
 
 # sbatch --array 0-11 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 0-11 evaluate_pie_lhc40d_cims.sh  # done
-# sbatch --array 0-11 evaluate_mf_lhc40d_cims.sh
+sbatch --array 0-11 evaluate_mf_lhc40d_cims.sh
 # sbatch --array 0-11 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 0-11 evaluate_flow_lhc_cims.sh  # done
@@ -34,9 +34,8 @@
 
 # sbatch --array 12-35 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 12-35 evaluate_pie_lhc40d_cims.sh  # done
-# sbatch --array 12-35 evaluate_mf_lhc40d_cims.sh
-# sbatch --array 12-23 evaluate_emf_lhc40d_cims.sh
-# sbatch --array 24-35 evaluate_emf_lhc40d_cims.sh
+sbatch --array 12-35 evaluate_mf_lhc40d_cims.sh
+# sbatch --array 12-35 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 12-35 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 12-35 evaluate_pie_lhc_cims.sh  # done
@@ -61,14 +60,12 @@
 # sbatch --array 36-59 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 36-59 evaluate_pie_lhc40d_cims.sh  # done
 # sbatch --array 36-59 evaluate_mf_lhc40d_cims.sh
-# sbatch --array 36-47 evaluate_emf_lhc40d_cims.sh
-# sbatch --array 48-59 evaluate_emf_lhc40d_cims.sh
+# sbatch --array 36-59 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 36-59 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 36-59 evaluate_pie_lhc_cims.sh  # done
 # sbatch --array 36-59 evaluate_mf_lhc_cims.sh  # done
-# sbatch --array 36-47 evaluate_emf_lhc_cims.sh
-# sbatch --array 48-59 evaluate_emf_lhc_cims.sh
+# sbatch --array 36-59 evaluate_emf_lhc_cims.sh
 
 # sbatch --array 36-59 evaluate_flow_lhc_long_cims.sh
 # sbatch --array 36-59 evaluate_pie_lhc_long_cims.sh
