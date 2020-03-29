25134ad05bf91480f2baf96c6156702948481f54
Johann Brehmer <mail@johannbrehmer.de>
2020-03-29 18:34:07 -0400

    Think globally, test your model locally

--- experiments/cluster/test_local2.sh
+++ experiments/cluster/test_local2.sh
@@ -4,4 +4,5 @@ source activate ml
 dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $dir/experiments
 
-python -u evaluate.py --modelname alternate_march --dataset power --algorithm emf --splinebins 10 --splinerange 6. --gridresolution 101 -i 0 --dir $dir
+python -u evaluate.py --modelname sequential_march --dataset power --algorithm emf --splinebins 10 --splinerange 6. --gridresolution 101 -i 0 --dir $dir
+python -u evaluate.py --modelname sequential_march --dataset power --algorithm mf --splinebins 10 --splinerange 6. --gridresolution 101 -i 0 --dir $dir
