4cac0ad205ac8537d4bbb94fdba78fe744a6f302
Johann Brehmer <mail@johannbrehmer.de>
2020-03-30 10:28:24 -0400

    Feature and result gazing

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -9,8 +9,8 @@
 
 # sbatch --array 0-11 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 0-11 evaluate_pie_lhc40d_cims.sh  # done
-# sbatch --array 0-11 evaluate_mf_lhc40d_cims.sh  # submitted
-# sbatch --array 0-11 evaluate_emf_lhc40d_cims.sh
+# sbatch --array 0-11 evaluate_mf_lhc40d_cims.sh  # done
+# sbatch --array 0-11 evaluate_emf_lhc40d_cims.sh  # NaNs
 
 # sbatch --array 0-11 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 0-11 evaluate_pie_lhc_cims.sh  # done
@@ -31,8 +31,9 @@
 
 # sbatch --array 12-35 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 12-35 evaluate_pie_lhc40d_cims.sh  # done
-# sbatch --array 12-35 evaluate_mf_lhc40d_cims.sh  # submitted
-sbatch --array 12-35 evaluate_emf_lhc40d_cims.sh
+# sbatch --array 12-35 evaluate_mf_lhc40d_cims.sh  # done
+sbatch --array 12-23 evaluate_emf_lhc40d_cims.sh
+# sbatch --array 24-35 evaluate_emf_lhc40d_cims.sh  # NaNs
 
 # sbatch --array 12-35 evaluate_flow_lhc_cims.sh  # done
 # sbatch --array 12-35 evaluate_pie_lhc_cims.sh  # done
@@ -53,7 +54,7 @@ sbatch --array 12-35 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 36-59 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 36-59 evaluate_pie_lhc40d_cims.sh  # done
-# sbatch --array 36-59 evaluate_mf_lhc40d_cims.sh  # submitted
+# sbatch --array 36-59 evaluate_mf_lhc40d_cims.sh  # done
 # sbatch --array 36-59 evaluate_emf_lhc40d_cims.sh
 
 # sbatch --array 36-59 evaluate_flow_lhc_cims.sh  # done
