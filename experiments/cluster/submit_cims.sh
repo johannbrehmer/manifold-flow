3329a7ceb3ae14783cf61a223e074e45aa6c2c6b
Johann Brehmer <mail@johannbrehmer.de>
2020-04-06 10:49:40 -0400

    Prepared next round of eval jobs on CIMS

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-sbatch --array 0-59 evaluate_flow_lhc2d_cims.sh
-# sbatch --array 60-119 evaluate_flow_lhc2d_cims.sh
+# sbatch --array 0-59 evaluate_flow_lhc2d_cims.sh  # done
+sbatch --array 60-119 evaluate_flow_lhc2d_cims.sh
 
-sbatch --array 0-59 evaluate_flow_lhc40d_cims.sh
-# sbatch --array 60-119 evaluate_flow_lhc40d_cims.sh
+# sbatch --array 0-59 evaluate_flow_lhc40d_cims.sh  # done
+sbatch --array 60-119 evaluate_flow_lhc40d_cims.sh
 
-sbatch --array 0-59 evaluate_pie_lhc40d_cims.sh
-# sbatch --array 60-119 evaluate_pie_lhc40d_cims.sh
+# sbatch --array 0-59 evaluate_pie_lhc40d_cims.sh  # done
+sbatch --array 60-119 evaluate_pie_lhc40d_cims.sh
 
-sbatch --array 0-35 evaluate_mfs_lhc40d_cims.sh
-# sbatch --array 36-47 evaluate_mfs_lhc40d_cims.sh
-sbatch --array 48-71 evaluate_mfs_lhc40d_cims.sh
-# sbatch --array 72-119 evaluate_mfs_lhc40d_cims.sh
+# sbatch --array 0-35 evaluate_mfs_lhc40d_cims.sh  # done
+sbatch --array 36-47 evaluate_mfs_lhc40d_cims.sh
+# sbatch --array 48-71 evaluate_mfs_lhc40d_cims.sh  # done
+sbatch --array 72-119 evaluate_mfs_lhc40d_cims.sh
 
-# sbatch --array 0-119 evaluate_mfa_lhc40d_cims.sh
+sbatch --array 0-119 evaluate_mfa_lhc40d_cims.sh
 
-# sbatch --array 0-119 evaluate_emfs_lhc40d_cims.sh
+sbatch --array 0-119 evaluate_emfs_lhc40d_cims.sh
 
-# sbatch --array 0-119 evaluate_emfa_lhc40d_cims.sh
+sbatch --array 0-119 evaluate_emfa_lhc40d_cims.sh
