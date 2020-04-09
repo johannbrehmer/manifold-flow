eb834196f7a8f08eedb3c44d6dd134865d7e7229
Johann Brehmer <mail@johannbrehmer.de>
2020-04-09 09:51:29 -0400

    Updated submit_cims.sh for last (?) batch of jobs

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -2,39 +2,40 @@
 
 # sbatch --array 0-59 evaluate_flow_lhc2d_cims.sh  # done
 # sbatch --array 60-95 evaluate_flow_lhc2d_cims.sh  # submitted
-sbatch --array 96-107 evaluate_flow_lhc2d_cims.sh
-# sbatch --array 108-119 evaluate_flow_lhc2d_cims.sh
+# sbatch --array 96-107 evaluate_flow_lhc2d_cims.sh  # submitted
+sbatch --array 108-119 evaluate_flow_lhc2d_cims.sh
 
 # sbatch --array 0-59 evaluate_flow_lhc40d_cims.sh  # done
 # sbatch --array 60-95 evaluate_flow_lhc40d_cims.sh  # submitted
-sbatch --array 96-107 evaluate_flow_lhc40d_cims.sh
-# sbatch --array 108-119 evaluate_flow_lhc40d_cims.sh
+# sbatch --array 96-107 evaluate_flow_lhc40d_cims.sh  # submitted
+sbatch --array 108-119 evaluate_flow_lhc40d_cims.sh
 
 # sbatch --array 0-59 evaluate_pie_lhc40d_cims.sh  # done
 # sbatch --array 60-95 evaluate_pie_lhc40d_cims.sh  # submitted
-sbatch --array 96-107 evaluate_pie_lhc40d_cims.sh
-# sbatch --array 108-119 evaluate_pie_lhc40d_cims.sh
+# sbatch --array 96-107 evaluate_pie_lhc40d_cims.sh  # submitted
+sbatch --array 108-119 evaluate_pie_lhc40d_cims.sh
 
 # sbatch --array 0-35 evaluate_mfs_lhc40d_cims.sh  # done
-# sbatch --array 36-47 evaluate_mfs_lhc40d_cims.sh
+sbatch --array 36-47 evaluate_mfs_lhc40d_cims.sh
 # sbatch --array 48-71 evaluate_mfs_lhc40d_cims.sh  # done
 # sbatch --array 72-107 evaluate_mfs_lhc40d_cims.sh  # submitted
-# sbatch --array 108-119 evaluate_mfs_lhc40d_cims.sh
+sbatch --array 108-119 evaluate_mfs_lhc40d_cims.sh
 
 # sbatch --array 0-35 evaluate_mfa_lhc40d_cims.sh  # submitted
-# sbatch --array 36-47 evaluate_mfa_lhc40d_cims.sh
+sbatch --array 36-47 evaluate_mfa_lhc40d_cims.sh
 # sbatch --array 48-83 evaluate_mfa_lhc40d_cims.sh  # submitted
-# sbatch --array 84-119 evaluate_mfa_lhc40d_cims.sh
+sbatch --array 84-119 evaluate_mfa_lhc40d_cims.sh
 
 # sbatch --array 0-11 evaluate_emfs_lhc40d_cims.sh  # submitted
-# sbatch --array 12-23 evaluate_emfs_lhc40d_cims.sh
+sbatch --array 12-23 evaluate_emfs_lhc40d_cims.sh
 # sbatch --array 24-95 evaluate_emfs_lhc40d_cims.sh  # submitted
-sbatch --array 96-107 evaluate_emfs_lhc40d_cims.sh
-# sbatch --array 108-119 evaluate_emfs_lhc40d_cims.sh
+# sbatch --array 96-107 evaluate_emfs_lhc40d_cims.sh  # submitted
+sbatch --array 108-119 evaluate_emfs_lhc40d_cims.sh
 
-# sbatch --array 0-11 evaluate_emfa_lhc40d_cims.sh
+sbatch --array 0-11 evaluate_emfa_lhc40d_cims.sh
 # sbatch --array 12-23 evaluate_emfa_lhc40d_cims.sh  # submitted
-sbatch --array 24-47 evaluate_emfa_lhc40d_cims.sh
+# sbatch --array 24-47 evaluate_emfa_lhc40d_cims.sh  # submitted
 # sbatch --array 48-59 evaluate_emfa_lhc40d_cims.sh  # submitted
 sbatch --array 60-71 evaluate_emfa_lhc40d_cims.sh
-# sbatch --array 72-119 evaluate_emfa_lhc40d_cims.sh
+# sbatch --array 72-83 evaluate_emfa_lhc40d_cims.sh  # submitted
+sbatch --array 84-119 evaluate_emfa_lhc40d_cims.sh
