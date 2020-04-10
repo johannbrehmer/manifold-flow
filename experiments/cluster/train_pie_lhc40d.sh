4e1337242fd2bfadde40ce59aad21473bafd917a
Johann Brehmer <mail@johannbrehmer.de>
2020-04-10 15:21:55 -0400

    Updated paths in cluster scripts

--- experiments/cluster/train_pie_lhc40d.sh
+++ experiments/cluster/train_pie_lhc40d.sh
@@ -11,7 +11,7 @@
 module load cuda/10.1.105
 source activate ml
 export OMP_NUM_THREADS=1
-cd /scratch/jb6504/manifold-flow/experiments
+cd /scratch/jb6504/scandal-mf/experiments
 
 # python -u train.py -c cluster/configs/train_lhc_may.config --modelname may --algorithm pie -i ${SLURM_ARRAY_TASK_ID}
 python -u train.py -c cluster/configs/train_lhc_may.config --modelname scandal_may --algorithm pie --scandal 5 -i ${SLURM_ARRAY_TASK_ID}
