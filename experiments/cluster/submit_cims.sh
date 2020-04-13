f67fd412cf78a40d51f1649132ac61ccfbf2a73f
Johann Brehmer <mail@johannbrehmer.de>
2020-04-13 10:57:41 -0400

    Prepared eval scripts for SCANDAL runs

--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -3,6 +3,8 @@
 # Last update master: April 13, 10:00
 # Last update scandal: --
 
+
+
 # sbatch --array 0-107 evaluate_flow_lhc2d_cims.sh  # done
 sbatch --array 108-119 evaluate_flow_lhc2d_cims.sh
 
@@ -33,3 +35,19 @@ sbatch --array 0-11 evaluate_emfa_lhc40d_cims.sh
 sbatch --array 60-71 evaluate_emfa_lhc40d_cims.sh
 # sbatch --array 72-83 evaluate_emfa_lhc40d_cims.sh  # done
 sbatch --array 84-119 evaluate_emfa_lhc40d_cims.sh
+
+
+
+# sbatch --array 0-119 evaluate_flow_scandal_lhc2d_cims.sh
+
+# sbatch --array 0-119 evaluate_flow_scandal_lhc40d_cims.sh
+
+# sbatch --array 0-119 evaluate_pie_scandal_lhc40d_cims.sh
+
+# sbatch --array 0-119 evaluate_mfs_scandal_lhc40d_cims.sh
+
+# sbatch --array 0-119 evaluate_mfa_scandal_lhc40d_cims.sh
+
+# sbatch --array 0-119 evaluate_emfs_scandal_lhc40d_cims.sh
+
+# sbatch --array 0-119 evaluate_emfa_scandal_lhc40d_cims.sh
