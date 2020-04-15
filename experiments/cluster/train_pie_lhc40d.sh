794ba0f1a479cf8d7a84b1d7d6aa766d3cdf09cf
Johann Brehmer <mail@johannbrehmer.de>
2020-04-15 12:01:01 -0400

    Prepared fully conditional PIE training

--- experiments/cluster/train_pie_lhc40d.sh
+++ experiments/cluster/train_pie_lhc40d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=t-pie-l
-#SBATCH --output=log_train_pie_lhc_%a.log
+#SBATCH --output=log_train_pie2_lhc_%a.log
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32GB
@@ -13,5 +13,8 @@ source activate ml
 export OMP_NUM_THREADS=1
 cd /scratch/jb6504/scandal-mf/experiments
 
+python -u train.py -c cluster/configs/train_lhc_may.config --modelname conditionalmanifold_may --algorithm pie -i ${SLURM_ARRAY_TASK_ID}
+# python -u train.py -c cluster/configs/train_lhc_may.config --modelname conditionalmanifold_scandal_may --algorithm pie --scandal 5 -i ${SLURM_ARRAY_TASK_ID}
+
 # python -u train.py -c cluster/configs/train_lhc_may.config --modelname may --algorithm pie -i ${SLURM_ARRAY_TASK_ID}
-python -u train.py -c cluster/configs/train_lhc_may.config --modelname scandal_may --algorithm pie --scandal 5 -i ${SLURM_ARRAY_TASK_ID}
+# python -u train.py -c cluster/configs/train_lhc_may.config --modelname scandal_may --algorithm pie --scandal 5 -i ${SLURM_ARRAY_TASK_ID}
