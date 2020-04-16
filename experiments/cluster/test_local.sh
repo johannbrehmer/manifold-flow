fa388707e747d7a01089a56fa280adfcc02ac82c
Johann Brehmer <mail@johannbrehmer.de>
2020-04-16 14:53:27 -0400

    Updated config files

--- experiments/cluster/test_local.sh
+++ experiments/cluster/test_local.sh
@@ -4,4 +4,4 @@ conda activate ml
 dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $dir/experiments
 
-python -u train.py -c cluster/configs/train_celeba_april.config --algorithm flow --debug --dir $dir
+python -u train.py -c cluster/configs/train_mfmf_celeba_april.config --algorithm flow --debug --dir $dir
--- experiments/cluster/test_local2.sh
+++ experiments/cluster/test_local2.sh
@@ -4,4 +4,4 @@ conda activate ml
 dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $dir/experiments
 
-python -u train.py -c cluster/configs/train_celeba_april.config --algorithm pie --debug --dir $dir
+python -u train.py -c cluster/configs/train_flow_celeba_april.config --algorithm pie --debug --dir $dir
