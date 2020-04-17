469355c2c0df2f6eb173e08164da58fbcc733bf4
Johann Brehmer <mail@johannbrehmer.de>
2020-04-17 10:53:47 -0400

    First training scripts for image data

---
+++ experiments/cluster/train_pie_celeba.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=t-pie-l
+#SBATCH --output=log_train_pie_celeba.log
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32GB
+#SBATCH --time=7-00:00:00
+#SBATCH --gres=gpu:4
+
+module load cuda/10.1.105
+source activate ml
+export OMP_NUM_THREADS=1
+cd /scratch/jb6504/image-mf/experiments
+
+python -u train.py -c cluster/configs/train_flow_celeba_may.config --algorithm pie
