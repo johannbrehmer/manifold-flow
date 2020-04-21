6bd5573af03ead02d5c3ccc7c5f2329b9d7d679f
Johann Brehmer <mail@johannbrehmer.de>
2020-04-21 17:23:31 -0400

    Let's go, param scan on cluster

--- experiments/cluster/paramscan_mf_lhc.sh
+++ experiments/cluster/paramscan_mf_lhc.sh
@@ -6,11 +6,11 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32GB
 #SBATCH --time=7-00:00:00
-##SBATCH --gres=gpu:1
+## SBATCH --gres=gpu:1
 
 module load cuda/10.1.105
 source activate ml
 export OMP_NUM_THREADS=1
 cd /scratch/jb6504/manifold-flow/experiments
 
-python -u paramscan.py --paramscanstudyname mfpower${SLURM_ARRAY_TASK_ID}
+python -u paramscan.py -c cluster/configs/paramscan_lhc_may.config
