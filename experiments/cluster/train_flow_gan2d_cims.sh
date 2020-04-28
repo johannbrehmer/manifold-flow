f46d778ae98371ad013088f5583819dc1b32e94c
Johann Brehmer <mail@johannbrehmer.de>
2020-04-28 10:06:29 -0400

    Updated GPU requirements for CIMS jobs

--- experiments/cluster/train_flow_gan2d_cims.sh
+++ experiments/cluster/train_flow_gan2d_cims.sh
@@ -6,7 +6,8 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32GB
 #SBATCH --time=2-00:00:00
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=gpu_12gb
 
 conda activate ml
 export PATH="/home/brehmer/miniconda3/envs/ml/bin/:$PATH"
