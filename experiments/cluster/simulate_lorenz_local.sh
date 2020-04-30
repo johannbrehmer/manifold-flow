bb962868dcfb02720b8da3b53caaf34080515bfc
Johann Brehmer <mail@johannbrehmer.de>
2020-04-30 11:34:04 -0400

    Fiddling with plotting the Lorenz attractor manifold

--- experiments/cluster/simulate_lorenz_local.sh
+++ experiments/cluster/simulate_lorenz_local.sh
@@ -16,4 +16,4 @@ conda activate ml
 dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $dir/experiments
 
-python -u generate_data.py --dataset lorenz --train 100000 --test 10000 --dir $dir
+python -u generate_data.py --dataset lorenz --train 1000000 --test 10000 --dir $dir
