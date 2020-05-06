db133e10472b9b3a9c7d72cf9f5ce376154c17fb
Johann Brehmer <mail@johannbrehmer.de>
2020-05-06 11:28:47 -0400

    More cleanup

--- experiments/cluster/train_mf_lorenz_local.sh
+++ experiments/cluster/train_mf_lorenz_local.sh
@@ -4,4 +4,5 @@ conda activate ml
 dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $dir/experiments
 
-python -u train.py -c cluster/configs/train_mfmf_lorenz_may.config
+# python -u train.py -c cluster/configs/train_mfmf_lorenz_april.config --modelname april2
+python -u train.py -c cluster/configs/train_mfmf_lorenz_may.config --modelname may2
