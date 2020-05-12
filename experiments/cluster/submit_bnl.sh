4090bad27457ab5477ad72a145fad512376ad118
Johann Brehmer <mail@johannbrehmer.de>
2020-05-12 16:37:44 -0400

    BNL script stuffs

--- experiments/cluster/submit_bnl.sh
+++ experiments/cluster/submit_bnl.sh
@@ -4,7 +4,7 @@
 # IMDb training jobs
 ################################################################################
 
+# sbatch --array 0-0 train_flow_imdb_bnl.sh
 sbatch --array 0-0 train_mf_imdb_bnl.sh
-sbatch --array 0-0 train_flow_imdb_bnl.sh
-# sbatch --array 0-0 train_emf_imdb_bnl.sh
-# sbatch --array 0-0 train_pie_imdb_bnl.sh
+sbatch --array 0-0 train_emf_imdb_bnl.sh
+sbatch --array 0-0 train_pie_imdb_bnl.sh
