d302fc7f0d0742846a6fae6f2dd5d7154832af31
Johann Brehmer <mail@johannbrehmer.de>
2020-05-16 11:16:23 -0400

    Final GAN2D results

--- experiments/cluster/evaluate_emf_gan2d_local.sh
+++ experiments/cluster/evaluate_emf_gan2d_local.sh
@@ -4,7 +4,7 @@ conda activate ml
 dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $dir/experiments
 
-for i in 5 6 7 8
+for i in 9
 do
     python evaluate.py -c configs/evaluate_mf_gan2d_april.config --algorithm emf -i $i
 done
