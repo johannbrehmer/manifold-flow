248cee0226fb34d33bbf69f9bdcd8bafcaa787ba
Johann Brehmer <mail@johannbrehmer.de>
2020-05-19 10:30:06 -0400

    New LHC eval setup

---
+++ experiments/cluster/evaluate_emf_scandal_lhc_cims.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=e-emfs-l
+#SBATCH --output=log_evaluate_emf_lhc_scandal_%a.log
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16GB
+#SBATCH --time=1-00:00:00
+# #SBATCH --gres=gpu:1
+
+conda activate ml
+export PATH="/home/brehmer/miniconda3/envs/ml/bin/:$PATH"
+export OMP_NUM_THREADS=1
+dir=/data/brehmer/manifold-flow
+cd $dir/experiments
+
+run=$((SLURM_ARRAY_TASK_ID / 12))
+task=$((SLURM_ARRAY_TASK_ID % 12))
+chain=$((task / 3))
+true=$((task % 3))
+echo "SLURM_ARRAY_TASK_ID = ${SLURM_ARRAY_TASK_ID}, true = ${true}, chain = ${chain}, run = ${run}"
+
+python -u evaluate.py -c configs/evaluate_mf_lhc_june.config --algorithm emf --modelname scandal_june -i $run --trueparam $true --chain $chain
--- experiments/cluster/submit_cims.sh
+++ experiments/cluster/submit_cims.sh
@@ -4,52 +4,17 @@
 # LHC eval jobs
 ################################################################################
 
-# Last update master: April 15, 9:40
-# Last update scandal: May 1, 10:30
 
-# sbatch --array 0-119 evaluate_flow_lhc2d_cims.sh  # done
-# sbatch --array 0-119 evaluate_flow_lhc40d_cims.sh  # done
-# sbatch --array 0-119 evaluate_pie_lhc40d_cims.sh  # done
-# sbatch --array 0-119 evaluate_pie_cm_lhc40d_cims.sh
-# sbatch --array 0-119 evaluate_mfs_lhc40d_cims.sh  # done
-# sbatch --array 0-119 evaluate_mfa_lhc40d_cims.sh  # done
-# sbatch --array 0-119 evaluate_emfs_lhc40d_cims.sh  # done
-# sbatch --array 0-119 evaluate_emfa_lhc40d_cims.sh  # done
-
-# sbatch --array 0-107 evaluate_flow_scandal_lhc2d_cims.sh  # done
-# sbatch --array 108-119 evaluate_flow_scandal_lhc2d_cims.sh
-
-# sbatch --array 0-107 evaluate_flow_scandal_lhc40d_cims.sh  # done
-# sbatch --array 108-119 evaluate_flow_scandal_lhc40d_cims.sh
-
-# sbatch --array 0-95 evaluate_pie_scandal_lhc40d_cims.sh  # done
-# sbatch --array 96-119 evaluate_pie_scandal_lhc40d_cims.sh
-
-# sbatch --array 0-119 evaluate_pie_cm_scandal_lhc40d_cims.sh
-
-# sbatch --array 0-83 evaluate_mfs_scandal_lhc40d_cims.sh  # done
-# sbatch --array 84-95 evaluate_mfs_scandal_lhc40d_cims.sh
-# sbatch --array 96-107 evaluate_mfs_scandal_lhc40d_cims.sh  # done
-# sbatch --array 108-119 evaluate_mfs_scandal_lhc40d_cims.sh
-
-# sbatch --array 0-59 evaluate_mfa_scandal_lhc40d_cims.sh
-# sbatch --array 60-95 evaluate_mfa_scandal_lhc40d_cims.sh  # done
-# sbatch --array 96-119 evaluate_mfa_scandal_lhc40d_cims.sh
-
-# sbatch --array 0-35 evaluate_emfs_scandal_lhc40d_cims.sh  # done
-# sbatch --array 36-119 evaluate_emfs_scandal_lhc40d_cims.sh
-# sbatch --array 48-59 evaluate_emfs_scandal_lhc40d_cims.sh  # done
-# sbatch --array 60-71 evaluate_emfs_scandal_lhc40d_cims.sh
-# sbatch --array 72-107 evaluate_emfs_scandal_lhc40d_cims.sh  # done
-# sbatch --array 108-119 evaluate_emfs_scandal_lhc40d_cims.sh
-
-# sbatch --array 0-11 evaluate_emfa_scandal_lhc40d_cims.sh
-# sbatch --array 12-23 evaluate_emfa_scandal_lhc40d_cims.sh  # done
-# sbatch --array 24-35 evaluate_emfa_scandal_lhc40d_cims.sh
-# sbatch --array 36-47 evaluate_emfa_scandal_lhc40d_cims.sh  # done
-# sbatch --array 48-59 evaluate_emfa_scandal_lhc40d_cims.sh
-# sbatch --array 60-83 evaluate_emfa_scandal_lhc40d_cims.sh  # done
-# sbatch --array 84-119 evaluate_emfa_scandal_lhc40d_cims.sh
+sbatch --array 0-119 evaluate_mf_lhc_cims.sh
+sbatch --array 0-119 evaluate_mf_scandal_lhc_cims.sh
+sbatch --array 0-119 evaluate_emf_lhc_cims.sh
+sbatch --array 0-119 evaluate_emf_scandal_lhc_cims.sh
+sbatch --array 0-119 evaluate_flow_lhc_cims.sh
+sbatch --array 0-119 evaluate_flow_scandal_lhc_cims.sh
+sbatch --array 0-119 evaluate_pie_lhc_cims.sh
+sbatch --array 0-119 evaluate_pie_scandal_lhc_cims.sh
+sbatch --array 0-119 evaluate_pie_cm_lhc_cims.sh
+sbatch --array 0-119 evaluate_pie_cm_scandal_lhc_cims.sh
 
 
 
@@ -57,7 +22,7 @@
 # GAN2D train jobs
 ################################################################################
 
-sbatch --array 0-0 train_mf_gan2d_cims.sh
-sbatch --array 0-0 train_emf_gan2d_cims.sh
-sbatch --array 0-0 train_pie_gan2d_cims.sh
-sbatch --array 0-0 train_flow_gan2d_cims.sh
+#sbatch --array 0-9 train_mf_gan2d_cims.sh
+#sbatch --array 0-9 train_emf_gan2d_cims.sh
+#sbatch --array 0-9 train_pie_gan2d_cims.sh
+#sbatch --array 0-9 train_flow_gan2d_cims.sh
