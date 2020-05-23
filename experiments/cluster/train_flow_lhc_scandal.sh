57d455d378f1892610d6080efffe3b660f182dc2
Johann Brehmer <mail@johannbrehmer.de>
2020-05-23 08:22:59 -0400

    Updated scandal script to resume training on Prince

--- experiments/cluster/train_flow_lhc_scandal.sh
+++ experiments/cluster/train_flow_lhc_scandal.sh
@@ -11,7 +11,7 @@
 # module load cuda/10.1.105
 source activate ml
 export OMP_NUM_THREADS=1
-cd /scratch/jb6504/manifold-flow/experiments
+cd /scratch/jb6504/manifold-flow2/experiments
 
-python -u train.py -c configs/train_flow_lhc_june.config --modelname scandal_june --scandal 2 --resume 40 -i ${SLURM_ARRAY_TASK_ID}
-# python -u train.py -c configs/train_flow_lhc_june.config --modelname scandal_june --scandal 2 --resume 30 -i ${SLURM_ARRAY_TASK_ID}
+python -u train.py -c configs/train_flow_lhc_june.config --modelname scandal_june --scandal 2 --resume 40 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow2
+# python -u train.py -c configs/train_flow_lhc_june.config --modelname scandal_june --scandal 2 --resume 30 -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow2
