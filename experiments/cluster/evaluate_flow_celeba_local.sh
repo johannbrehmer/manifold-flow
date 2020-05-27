45d6ed85b853c00786b32628a924f1a4a4d10227
Johann Brehmer <mail@johannbrehmer.de>
2020-05-27 17:34:07 -0400

    CelebA projections

--- experiments/cluster/evaluate_flow_celeba_local.sh
+++ experiments/cluster/evaluate_flow_celeba_local.sh
@@ -4,7 +4,8 @@ conda activate ml
 dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $dir/experiments
 
-for i in 0 1 2
+for i in 0 1
 do
-    python evaluate.py -c configs/evaluate_flow_celeba_april.config -i $i
+    # python evaluate.py -c configs/evaluate_flow_celeba_april.config -i $i
+    python evaluate.py -c configs/evaluate_flow_celeba_april.config --modelname long_april -i $i
 done
