967a5afa9004ef58a10f84325754c37a2cfa3f98
Johann Brehmer <mail@johannbrehmer.de>
2020-05-28 18:57:33 -0400

    Fixed paths for running PIE on CelebA on Prince

--- experiments/cluster/train_pie_celeba.sh
+++ experiments/cluster/train_pie_celeba.sh
@@ -16,4 +16,4 @@ cd /scratch/jb6504/manifold-flow/experiments
 nvcc --version
 nvidia-smi
 
-python -u train.py -c configs/train_pie_celeba_april.config -i ${SLURM_ARRAY_TASK_ID}
+python -u train.py -c configs/train_pie_celeba_april.config -i ${SLURM_ARRAY_TASK_ID} --dir /scratch/jb6504/manifold-flow
