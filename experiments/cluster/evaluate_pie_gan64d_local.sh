990dcba07f2bb437ae2ef8ec51257bbe3d9c4ea9
Johann Brehmer <mail@johannbrehmer.de>
2020-05-29 11:06:15 -0400

    More GAN64D eval scripts

---
+++ experiments/cluster/evaluate_pie_gan64d_local.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+conda activate ml
+dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
+cd $dir/experiments
+
+for i in 0 1 2
+do
+    python evaluate.py -c configs/evaluate_pie_gan64d_april.config -i $i
+done
