836c1ce16475b15bc1e9ce68fb767c7ba2f5dfbc
Johann Brehmer <mail@johannbrehmer.de>
2020-06-01 18:59:56 -0400

    So many results

--- experiments/cluster/evaluate_mf_gan64d_local.sh
+++ experiments/cluster/evaluate_mf_gan64d_local.sh
@@ -4,7 +4,7 @@ conda activate ml
 dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $dir/experiments
 
-for i in 1 2  # Missing: 0
+for i in 0 1 2
 do
     python evaluate.py -c configs/evaluate_mf_gan64d_april.config -i $i
 done
