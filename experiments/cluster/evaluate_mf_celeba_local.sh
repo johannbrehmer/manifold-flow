d7614a7e67e1b4f90c4433c7e2361e8570d13470
Johann Brehmer <mail@johannbrehmer.de>
2020-06-02 10:17:20 -0400

    Cleaning up

--- experiments/cluster/evaluate_mf_celeba_local.sh
+++ experiments/cluster/evaluate_mf_celeba_local.sh
@@ -6,5 +6,6 @@ cd $dir/experiments
 
 for i in 0 1 2
 do
-    python evaluate.py -c configs/evaluate_mf_celeba_april.config -i $i
+    # python evaluate.py -c configs/evaluate_mf_celeba_april.config -i $i
+    python evaluate.py -c configs/evaluate_mf_celeba_april.config --modelname long_april -i $i
 done
