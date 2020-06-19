86f29164ec4874107d8ffe56b1c04e21a8aea290
Johann Brehmer <mail@johannbrehmer.de>
2020-06-19 13:49:40 -0400

    More animations

--- experiments/cluster/evaluate_mf_celeba_local.sh
+++ experiments/cluster/evaluate_mf_celeba_local.sh
@@ -4,7 +4,7 @@ conda activate ml
 dir=/Users/johannbrehmer/work/projects/manifold_flow/manifold-flow
 cd $dir/experiments
 
-for i in 0 1 2
+for i in 0
 do
     # python evaluate.py -c configs/evaluate_mf_celeba_april.config -i $i
     python evaluate.py -c configs/evaluate_mf_celeba_april.config -i $i --modellatentdim 128
